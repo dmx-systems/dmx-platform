43e7f46e94bf4e9a888cc6ed973c820998c0611b
Jörg Richter <jri@deepamehta.de>
2010-10-09 17:31:25 +0200

    Module deepamehta3-notes added to scripts/clone-modules.sh

--- scripts/clone-modules.sh
+++ scripts/clone-modules.sh
@@ -5,17 +5,17 @@ git clone git://github.com/jri/deepamehta3-core.git
 git clone git://github.com/jri/deepamehta3-coretypes.git
 git clone git://github.com/jri/deepamehta3-server.git
 git clone git://github.com/jri/deepamehta3-client.git
-git clone git://github.com/jri/deepamehta3-time.git
-git clone git://github.com/jri/deepamehta3-workspaces.git
-git clone git://github.com/jri/deepamehta3-topicmaps.git
-git clone git://github.com/jri/deepamehta3-typeeditor.git
-git clone git://github.com/jri/deepamehta3-iconpicker.git
 git clone git://github.com/jri/deepamehta3-contacts.git
-git clone git://github.com/jri/deepamehta3-projects.git
-git clone git://github.com/jri/deepamehta3-typesearch.git
 git clone git://github.com/jri/deepamehta3-files.git
 git clone git://github.com/jri/deepamehta3-filemanager.git
+git clone git://github.com/jri/deepamehta3-foldercanvas.git     # optional plugin
+git clone git://github.com/jri/deepamehta3-iconpicker.git
+git clone git://github.com/jri/deepamehta3-importer.git         # optional plugin
+git clone git://github.com/jri/deepamehta3-notes.git
+git clone git://github.com/jri/deepamehta3-projects.git
+git clone git://github.com/jri/deepamehta3-time.git
 git clone git://github.com/mukil/deepamehta3-topicimporter.git
-
-git clone git://github.com/jri/deepamehta3-foldercanvas.git
-git clone git://github.com/jri/deepamehta3-importer.git
+git clone git://github.com/jri/deepamehta3-topicmaps.git
+git clone git://github.com/jri/deepamehta3-typeeditor.git
+git clone git://github.com/jri/deepamehta3-typesearch.git
+git clone git://github.com/jri/deepamehta3-workspaces.git
