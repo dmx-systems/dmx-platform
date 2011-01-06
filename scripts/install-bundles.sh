9c82ae92d3df5ecc0e22abdf531452b97de48bba
Jörg Richter <jri@deepamehta.de>
2011-01-06 16:22:46 +0100

    scripts/install-bundles.sh added to support the build process.

---
+++ scripts/install-bundles.sh
@@ -0,0 +1,24 @@
+cd bundle
+
+curl -O http://www.apache.org/dist/felix/org.apache.felix.configadmin-1.2.8.jar
+curl -O http://www.apache.org/dist/felix/org.apache.felix.http.jetty-2.0.4.jar
+curl -O http://www.apache.org/dist/felix/org.apache.felix.fileinstall-3.1.2.jar
+
+curl -O http://www.deepamehta.de/maven2/org/apache/lucene/lucene-core/2.9.2/lucene.core-2.9.2.jar
+curl -O http://www.deepamehta.de/maven2/org/objectweb/asm/asm/3.2/asm-3.2.jar
+
+curl -O http://download.java.net/maven/2/org/jvnet/mimepull/1.4/mimepull-1.4.jar
+curl -O http://download.java.net/maven/2/com/sun/jersey/jersey-core/1.4/jersey-core-1.4.jar
+curl -O http://download.java.net/maven/2/com/sun/jersey/jersey-server/1.4/jersey-server-1.4.jar
+curl -O http://download.java.net/maven/2/com/sun/jersey/contribs/jersey-multipart/1.4/jersey-multipart-1.4.jar
+curl -O http://repo1.maven.org/maven2/org/codehaus/jettison/jettison/1.2/jettison-1.2.jar
+curl -O http://repo1.maven.org/maven2/org/codehaus/jackson/jackson-core-asl/1.6.1/jackson-core-asl-1.6.1.jar
+curl -O http://repo1.maven.org/maven2/org/codehaus/jackson/jackson-mapper-asl/1.6.1/jackson-mapper-asl-1.6.1.jar
+curl -O http://repo1.maven.org/maven2/org/codehaus/jackson/jackson-jaxrs/1.6.1/jackson-jaxrs-1.6.1.jar
+curl -O http://download.java.net/maven/2/com/sun/jersey/jersey-json/1.4/jersey-json-1.4.jar
+
+curl -O http://repo1.maven.org/maven2/org/apache/geronimo/specs/geronimo-jta_1.1_spec/1.1.1/geronimo-jta_1.1_spec-1.1.1.jar
+curl -O http://m2.neo4j.org/org/neo4j/neo4j-kernel/1.1/neo4j-kernel-1.1.jar
+curl -O http://m2.neo4j.org/org/neo4j/neo4j-index/1.1/neo4j-index-1.1.jar
+curl -O http://m2.neo4j.org/org/neo4j/neo4j-utils/1.1/neo4j-utils-1.1.jar
+curl -O http://m2.neo4j.org/org/neo4j/neo4j-meta-model/0.9-SNAPSHOT/neo4j-meta-model-0.9-20100804.080135-13.jar
