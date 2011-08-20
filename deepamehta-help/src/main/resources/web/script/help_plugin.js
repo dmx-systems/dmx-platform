f3a1e81b6165e635d865043d7875944db5616966
Jörg Richter <jri@deepamehta.de>
2011-08-21 01:00:24 +0200

    About box: add 6 more contributors (#70).
    See ticket 70.

--- deepamehta-help/src/main/resources/web/script/help_plugin.js
+++ deepamehta-help/src/main/resources/web/script/help_plugin.js
@@ -27,33 +27,46 @@ function help_plugin() {
                 '<tr>' +
                     '<td>' +
                         '<img src="/images/deepamehta-logo.png">' +
+                    '</td>' +
+                    '<td>' +
                         '<div class="field-label">Version</div>' +
                         '<div class="field-value">${project.version}</div>' +
                         '<div class="field-label">Copyright</div>' +
                         '<div class="field-value">2000-2011 Jörg Richter</div>' +
                         '<div class="field-label">License</div>' +
                         '<div class="field-value">GNU General Public License, v3</div>' +
+                        '<div class="field-label">Website</div>' +
+                        '<div class="field-value">' +
+                            '<a href="http://www.deepamehta.de/" target="_blank">www.deepamehta.de</a>' +
+                        '</div>' +
                     '</td>' +
                     '<td>' +
                         '<div class="field-label">Contributors</div>' +
                         '<div class="field-value">' +
+                            'Martin Delius<br>' +
                             'Andreas Gebhard<br>' +
                             'Danny Gräf<br>' +
                             'Annette Leeb<br>' +
+                            'Urs Lerch<br>' +
+                            'Matthias Melcher<br>' +
                             'Christiane Müller<br>' +
                             'Jürgen Neumann<br>' +
                             'Jurij Poelchau<br>' +
+                        '</div>' +
+                    '</td>' +
+                    '<td>' +
+                        '<div class="field-label" style="visibility: hidden">Contributors</div>' +
+                        '<div class="field-value">' +
                             'Ingo Rau<br>' +
                             'Malte Reißig<br>' +
                             'Jörg Richter<br>' +
+                            'Vincent Scheffler<br>' +
                             'Enrico Schnepel<br>' +
+                            'Thilo Schönnemann<br>' +
                             'Matthias Staps<br>' +
                             'Andreas Wichmann<br>' +
-                            '... <i>and many others</i><br>' +
-                        '</div>' +
-                        '<div class="field-label">Website</div>' +
-                        '<div class="field-value">' +
-                            '<a href="http://www.deepamehta.de/" target="_blank">www.deepamehta.de</a>' +
+                            'Torsten Ziegler<br>' +
+                            '... <i>and many others</i>' +
                         '</div>' +
                     '</td>' +
                 '</tr>' +
