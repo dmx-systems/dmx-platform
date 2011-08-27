324f3639d219471cddcaa9478fa6d4335bb59d36
Jörg Richter <jri@deepamehta.de>
2011-08-27 03:43:52 +0200

    Runtime: add "exec" to deepamehta-linux.sh (#83)
    Close ticket 83.

--- deepamehta-distribution/script/deepamehta-linux.sh
+++ deepamehta-distribution/script/deepamehta-linux.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd "$(dirname "$0")"
-java -Dfile.encoding=UTF-8 -Dfelix.system.properties=file:conf/config.properties -jar bin/felix.jar
+exec java -Dfile.encoding=UTF-8 -Dfelix.system.properties=file:conf/config.properties -jar bin/felix.jar
