2b16df392ae0f3f52364e9b63f71f54006e21377
Jörg Richter <jri@deepamehta.de>
2011-09-16 15:44:48 +0200

    Webclient fix: search results are sorted (#69).
    Close ticket 69.

--- deepamehta-webclient/src/main/resources/web/script/field_renderers/search_result_renderer.js
+++ deepamehta-webclient/src/main/resources/web/script/field_renderers/search_result_renderer.js
@@ -3,7 +3,7 @@ function SearchResultRenderer(topic, field) {
     this.render_field = function() {
         // retrieve search result
         var traversal_filter = {assoc_type_uri: "dm4.webclient.search_result_item"}
-        var result_items = dm4c.restc.get_related_topics(topic.id, traversal_filter)    // ### FIXME: sorting?
+        var result_items = dm4c.restc.get_related_topics(topic.id, traversal_filter, true)
         // render field label
         dm4c.render.field_label(field, " (" + result_items.length + ")")
         // render field value
