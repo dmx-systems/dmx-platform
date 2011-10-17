6a13f82a5dcc8ab5d886e524cb97d8b3eeaaa472
Jörg Richter <jri@deepamehta.de>
2011-10-17 14:05:24 +0200

    Begin of new feature: Facets (#109).
    Will be realized in separate module deepamehta-facets.

---
+++ deepamehta-geomaps/src/main/java/de/deepamehta/plugins/geomaps/GeomapsPlugin.java
@@ -0,0 +1,35 @@
+package de.deepamehta.plugins.geomaps;
+
+import de.deepamehta.core.Topic;
+import de.deepamehta.core.model.TopicModel;
+import de.deepamehta.core.service.Directive;
+import de.deepamehta.core.service.Directives;
+import de.deepamehta.core.service.Plugin;
+
+import java.util.logging.Logger;
+
+
+
+public class GeomapsPlugin extends Plugin {
+
+    // ---------------------------------------------------------------------------------------------- Instance Variables
+
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+    // -------------------------------------------------------------------------------------------------- Public Methods
+
+
+
+    // **************************************************
+    // *** Core Hooks (called from DeepaMehta 4 Core) ***
+    // **************************************************
+
+
+
+    @Override
+    public void postUpdateHook(Topic topic, TopicModel oldTopic, Directives directives) {
+        if (topic.getTypeUri().equals("dm4.contacts.address")) {
+            logger.info("### Updating \"Address\" " + topic);
+        }
+    }
+}
