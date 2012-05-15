8d2280ee836e58bc955c0bca2b1729b8b4308de5
Jörg Richter <jri@deepamehta.de>
2012-05-16 01:24:50 +0200

    Webclient/Type Editor: minor CSS tweaks.

--- modules/dm4-webclient/src/main/resources/web/script/page_renderers/association_renderer.js
+++ modules/dm4-webclient/src/main/resources/web/script/page_renderers/association_renderer.js
@@ -79,7 +79,7 @@ function AssociationRenderer() {
         var role_type_div = $("<div>")
             .append($("<div>").addClass("field-label").text("Role Type"))
             .append($("<div>").append(role_type_element))
-        dm4c.render.page($("<div>").addClass("assoc-role").addClass("box").addClass("level1")
+        dm4c.render.page($("<div>").addClass("assoc-role")
             .append(topic_div).append(role_type_div))
     }
 }
