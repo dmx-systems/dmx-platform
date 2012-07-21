275fa845ac6ed537eb0a41a08d41608e54c3f459
Jörg Richter <jri@deepamehta.de>
2012-07-21 19:33:42 +0200

    Webclient: fix Return key in textarea (#288,#206).
    Fixed in master.
    
    Close ticket 288, 206.

--- modules/dm4-webclient/src/main/resources/web/script/gui/page_panel.js
+++ modules/dm4-webclient/src/main/resources/web/script/gui/page_panel.js
@@ -101,9 +101,10 @@ function PagePanel() {
      */
     function do_process_key(event) {
         // dm4c.log("Key up " + event.which)
-        if (event.which == 13) {
+        if (event.which == 13 && event.target.nodeName == "INPUT" && event.target.type == "text") {
             var submit_button = $("#page-toolbar button[type=submit]")
-            // alert("do_process_key: submit button=\"" + submit_button.text() + "\"")
+            // alert("do_process_key:\nsubmit button=\"" + submit_button.text() +
+            //    "\"\ntarget.type=" + js.inspect(event.target.type))
             submit_button.click()
         }
         return false
--- modules/dm4-webclient/src/main/resources/web/script/renderers/simple_renderers/text_renderer.js
+++ modules/dm4-webclient/src/main/resources/web/script/renderers/simple_renderers/text_renderer.js
@@ -2,7 +2,11 @@ dm4c.add_simple_renderer("dm4.webclient.text_renderer", {
 
     render_info: function(page_model, parent_element) {
         dm4c.render.field_label(page_model, parent_element)
-        parent_element.append(js.render_text(page_model.value))
+        var text = js.render_text(page_model.value)
+        if (page_model.rows > 1) {
+            text = $("<p>").append(text)
+        }
+        parent_element.append(text)
     },
 
     render_form: function(page_model, parent_element) {
