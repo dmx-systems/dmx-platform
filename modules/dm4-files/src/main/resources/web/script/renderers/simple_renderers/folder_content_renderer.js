bc3e7110ebc35c6d05d0bd4f6fe0c6b252a0de92
Jörg Richter <jri@deepamehta.de>
2014-01-24 01:00:40 +0100

    Files plugin: fix sorting (#605).
    A Folder topic's content listing is sorted on all platforms.
    Sorting is case-insensitive. Files and folders are intermingled.
    
    Thanks to Carolina for reporting this issue!
    
    See #605.

--- modules/dm4-files/src/main/resources/web/script/renderers/simple_renderers/folder_content_renderer.js
+++ modules/dm4-files/src/main/resources/web/script/renderers/simple_renderers/folder_content_renderer.js
@@ -9,24 +9,32 @@ dm4c.add_simple_renderer("dm4.files.folder_content_renderer", {
         function render_content() {
             try {
                 var path = page_model.parent.object.get("dm4.files.path")
-                var items = dm4c.restc.get_directory_listing(path).items
-                var topics = []
-                for (var i = 0, item; item = items[i]; i++) {
-                    // error check
-                    if (item.kind != "file" && item.kind != "directory") {
-                        throw "FolderContentRendererError: item has unexpected kind (\"" + item.kind + "\")"
-                    }
-                    //
-                    var type_uri = item.kind == "file" ? "dm4.files.file" : "dm4.files.folder"
-                    topics.push({type_uri: type_uri, value: item.name, kind: item.kind, path: item.path})
-                }
-                // Note: topic_list() takes arbitrary objects as long as
-                // they have "type_uri" and "value" properties.
-                parent_element.append(dm4c.render.topic_list(topics, click_handler))
+                parent_element.append(dm4c.render.topic_list(get_topics(path), click_handler))
             } catch (e) {
                 parent_element.addClass("ui-state-error")
                 parent_element.append("FolderContentRendererError: " + e)
             }
+        }
+
+        function get_topics(path) {
+            var items = dm4c.restc.get_directory_listing(path).items
+            var topics = []
+            for (var i = 0, item; item = items[i]; i++) {
+                // error check
+                if (item.kind != "file" && item.kind != "directory") {
+                    throw "FolderContentRendererError: item has unexpected kind (\"" + item.kind + "\")"
+                }
+                // Note: dm4c.render.topic_list() takes arbitrary objects as long as they have "type_uri"
+                // and "value" properties.
+                var type_uri = item.kind == "file" ? "dm4.files.file" : "dm4.files.folder"
+                topics.push({type_uri: type_uri, value: item.name, kind: item.kind, path: item.path})
+            }
+            // Note: dm4c.restc.get_directory_listing() provides no ordering guarantee
+            topics.sort(function(topic_1, topic_2) {
+                return topic_1.value.toLowerCase() < topic_2.value.toLowerCase() ? -1 : 1
+            })
+            //
+            return topics
         }
 
         function click_handler(item, spot) {
