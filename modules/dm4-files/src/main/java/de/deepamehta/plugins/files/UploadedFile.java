0501d49954f610550afc2501e3ac9e8528df888d
Jörg Richter <jri@deepamehta.de>
2014-05-16 01:26:40 +0200

    Files module: access uploaded file content (#655).
    The Files APIs `de.deepamehta.plugins.files.UploadedFile` class provides 5 additional public methods for accessing the uploaded file's content and for writing it to disk.
    
    {{{
    String getString()
    String getString(String encoding)
    byte[] getBytes()
    InputStream getInputStream()
    
    void write(File file)
    }}}
    
    See #655

--- modules/dm4-files/src/main/java/de/deepamehta/plugins/files/UploadedFile.java
+++ modules/dm4-files/src/main/java/de/deepamehta/plugins/files/UploadedFile.java
@@ -3,6 +3,7 @@ package de.deepamehta.plugins.files;
 import org.apache.commons.fileupload.FileItem;
 
 import java.io.File;
+import java.io.InputStream;
 
 
 
@@ -10,9 +11,9 @@ import java.io.File;
  * An uploaded file.
  * <p>
  * Files are uploaded via the REST API by POSTing <code>multipart/form-data</code> to a resource method
- * which consumes <code>multipart/form-data</code> and has UploadedFile as the entity parameter.
+ * which consumes <code>multipart/form-data</code> and has UploadedFile as its entity parameter.
  * <p>
- * Client-side support: the public API of the <code>de.deepamehta.files</code> plugin provides a method
+ * Client-side support: the public API of the Files plugin provides a method
  * <code>dm4c.get_plugin("de.deepamehta.files").open_upload_dialog()</code> that allows the user to
  * choose and upload a file.</p>
  *
@@ -32,34 +33,86 @@ public class UploadedFile {
 
     // -------------------------------------------------------------------------------------------------- Public Methods
 
+    // === File Metadata ===
+
     /**
-     * Returns the original (client-side) file name.
+     * Returns the original filename in the client's filesystem, as provided by the browser (or other client software).
+     * In most cases, this will be the base file name, without path information. However, some clients, such as the
+     * Opera browser, do include path information.
      */
     public String getName() {
         return fileItem.getName();
     }
 
+    /**
+     * Returns the size of the file.
+     */
     public long getSize() {
         return fileItem.getSize();
     }
 
     /**
-     * Returns the content type passed by the browser or null if not defined.
+     * Returns the content type passed by the browser or <code>null</code> if not defined.
      */
     public String getMediaType() {
         return fileItem.getContentType();
     }
 
-    // ---
+    // === File Content ===
+
+    /**
+     * Returns the contents of the file item as a String, using the default character encoding.
+     */
+    public String getString() {
+        return fileItem.getString();
+    }
+
+    /**
+     * Returns the contents of the file item as a String, using the specified encoding.
+     */
+    public String getString(String encoding) {
+        try {
+            return fileItem.getString(encoding);    // throws UnsupportedEncodingException
+        } catch (Exception e) {
+            throw new RuntimeException("Getting the content of upload file failed (" + this + ")", e);
+        }
+    }
+
+    /**
+     * Returns the contents of the file item as an array of bytes.
+     */
+    public byte[] getBytes() {
+        return fileItem.get();
+    }
+
+    /**
+     * Returns an InputStream that can be used to retrieve the contents of the file.
+     */
+    public InputStream getInputStream() {
+        try {
+            return fileItem.getInputStream();       // throws IOException
+        } catch (Exception e) {
+            throw new RuntimeException("Getting input stream of upload file failed (" + this + ")", e);
+        }
+    }
+
+    // === Storage ===
+
+    /**
+     * A convenience method to write the uploaded file to disk.
+     */
+    public void write(File file) {
+        try {
+            fileItem.write(file);                   // throws Exception
+        } catch (Exception e) {
+            throw new RuntimeException("Writing upload file to disk failed (" + this + ")", e);
+        }
+    }
+
+    // ===
 
     @Override
     public String toString() {
         return "file \"" + getName() + "\" (" + getMediaType() + "), " + getSize() + " bytes";
-    }
-
-    // ----------------------------------------------------------------------------------------- Package Private Methods
-
-    void write(File file) throws Exception {
-        fileItem.write(file);   // throws Exception
     }
 }
