22c2b31f8188da86fba0e7be002dd09cf1f257d6
Jörg Richter <jri@deepamehta.de>
2014-06-07 17:06:18 +0200

    Topicmaps API: add convenience accessors (#660).
    `TopicViewmodel` has new convenience methods to access the standard view properties at server-side:
    {{{
    int getX()
    int getY()
    getVisibility()
    }}}
    
    See #660.

--- modules/dm4-topicmaps/src/main/java/de/deepamehta/plugins/topicmaps/model/TopicViewmodel.java
+++ modules/dm4-topicmaps/src/main/java/de/deepamehta/plugins/topicmaps/model/TopicViewmodel.java
@@ -9,7 +9,10 @@ import org.codehaus.jettison.json.JSONObject;
 
 /**
  * A topic viewmodel as contained in a topicmap viewmodel.
- * That is a generic topic model enriched by view properties ("x", "y", "visibility").
+ * <p>
+ * That is a generic topic model enriched by view properties. Standard view properties are "dm4.topicmaps.x",
+ * "dm4.topicmaps.y", and "dm4.topicmaps.visibility". Additional view properties can be added by plugins (by
+ * implementing a Viewmodel Customizer).
  */
 public class TopicViewmodel extends TopicModel {
 
@@ -30,6 +33,37 @@ public class TopicViewmodel extends TopicModel {
         return viewProps;
     }
 
+    // ---
+
+    /**
+     * Convencience method to access the "dm4.topicmaps.x" standard view property.
+     */
+    public int getX() {
+        // Note: coordinates can be both: double (through JavaScript) and integer (programmatically placed).
+        // ### TODO: store coordinates always as integers
+        Object x = viewProps.getObject("dm4.topicmaps.x");
+        return x instanceof Double ? ((Double) x).intValue() : (Integer) x;
+    }
+
+    /**
+     * Convencience method to access the "dm4.topicmaps.y" standard view property.
+     */
+    public int getY() {
+        // Note: coordinates can be both: double (through JavaScript) and integer (programmatically placed).
+        // ### TODO: store coordinates always as integers
+        Object y = viewProps.getObject("dm4.topicmaps.y");
+        return y instanceof Double ? ((Double) y).intValue() : (Integer) y;
+    }
+
+    /**
+     * Convencience method to access the "dm4.topicmaps.visibility" standard view property.
+     */
+    public boolean getVisibility() {
+        return viewProps.getBoolean("dm4.topicmaps.visibility");
+    }
+
+    // ---
+
     @Override
     public JSONObject toJSON() {
         try {
@@ -39,23 +73,5 @@ public class TopicViewmodel extends TopicModel {
         } catch (Exception e) {
             throw new RuntimeException("Serialization failed (" + this + ")", e);
         }
-    }
-
-    // ---
-
-    // ### not used
-    int getX() {
-        // Note: coordinates can be both: double (through JavaScript) and integer (programmatically placed).
-        // ### TODO: store coordinates always as integers
-        Object x = viewProps.getObject("x");
-        return x instanceof Double ? ((Double) x).intValue() : (Integer) x;
-    }
-
-    // ### used by GridPositioning
-    int getY() {
-        // Note: coordinates can be both: double (through JavaScript) and integer (programmatically placed).
-        // ### TODO: store coordinates always as integers
-        Object y = viewProps.getObject("y");
-        return y instanceof Double ? ((Double) y).intValue() : (Integer) y;
     }
 }
