4ba8738f7e1d23e48f7424562c7640662ccd2297
Jörg Richter <jri@deepamehta.de>
2014-06-14 23:35:46 +0200

    Workspaces model: add "Workspace Type" (#592).
    Migrations 2 and 3.
    
    See #592.

---
+++ modules/dm4-workspaces/src/main/java/de/deepamehta/plugins/workspaces/migrations/Migration3.java
@@ -0,0 +1,17 @@
+package de.deepamehta.plugins.workspaces.migrations;
+
+import de.deepamehta.core.model.AssociationDefinitionModel;
+import de.deepamehta.core.service.Migration;
+
+
+
+public class Migration3 extends Migration {
+
+    @Override
+    public void run() {
+        dms.getTopicType("dm4.workspaces.workspace").addAssocDef(
+            new AssociationDefinitionModel("dm4.core.aggregation_def",
+                "dm4.workspaces.workspace", "dm4.workspaces.type", "dm4.core.many", "dm4.core.one")
+        );
+    }
+}
