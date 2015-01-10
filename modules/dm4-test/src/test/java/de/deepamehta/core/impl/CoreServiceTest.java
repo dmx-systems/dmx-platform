dmx-systems/dmx-platform

commit 9d90a8bc7988cd979f8e64e1485a133914428108

Workspace assignment is a :1 relationship (#592).
MODEL CHANGE

The Workspace facet is modeled as single-valued: each object is assigned to *one* workspace.

Pending:
The respective migration.

Workspaces API

Re-assigning a workspace now works:
{{{
void assignToWorkspace(DeepaMehtaObject object, long workspaceId)
void assignTypeToWorkspace(Type type, long workspaceId)
}}}
The API has not changed (but the behavior has)

Facets API

2 new methods in `FacetValue`:
{{{
FacetValue putRef(long refTopicId)
FacetValue putRef(String refTopicUri)
}}}
Puts a topic reference for a single-valued facet. By-ID resp. by-URI.

See #592.

modules/dm4-help/src/main/resources/web/script/plugin.js
@@ -15,7 +15,7 @@ dm4c.add_plugin("de.deepamehta.help", function() {
                         '<div class="field-label">Version</div>' +
                         '<div>${project.version}</div>' +
                         '<div class="field-label">Release Date</div>' +
-                        '<div>Jan 5, 2015</div>' +
+                        '<div>Jan 7, 2015</div>' +
                         '<div class="field-label">Copyright</div>' +
                         '<div>2000-2015 Jörg Richter et al.</div>' +
                         '<div class="field-label">License</div>' +

modules/dm4-facets/src/main/java/de/deepamehta/plugins/facets/model/FacetValue.java
@@ -38,6 +38,8 @@ public class FacetValue extends ChildTopicsModel {
 
     // -------------------------------------------------------------------------------------------------- Public Methods
 
+    // === Single-valued Facets ===
+
     /**
      * Accesses a single-valued facet.
      */
@@ -80,7 +82,23 @@ public class FacetValue extends ChildTopicsModel {
     // ---
 
     /**
-     * Adds all the values to a multiple-valued facet.
+     * Puts a by-ID topic reference for a single-valued facet.
+     */
+    public FacetValue putRef(long refTopicId) {
+        return (FacetValue) putRef(childTypeUri, refTopicId);
+    }
+
+    /**
+     * Puts a by-URI topic reference for a single-valued facet.
+     */
+    public FacetValue putRef(String refTopicUri) {
+        return (FacetValue) putRef(childTypeUri, refTopicUri);
+    }
+
+    // === Multiple-valued Facets ===
+
+    /**
+     * Sets the values of a multiple-valued facet.
      */
     public FacetValue put(List<TopicModel> values) {
         return (FacetValue) put(childTypeUri, values);

modules/dm4-workspaces/src/main/java/de/deepamehta/plugins/workspaces/WorkspacesPlugin.java
@@ -308,11 +308,9 @@ public class WorkspacesPlugin extends PluginActivator implements WorkspacesServi
     private void _assignToWorkspace(DeepaMehtaObject object, long workspaceId) {
         try {
             // 1) create assignment association
-            // Note 1: we are refering to an existing workspace. So we must add a topic reference.
-            // Note 2: workspace_facet is a multi-facet. So we must call addRef() (as opposed to putRef()).
-            // ### TODO: redefine workspace_facet as a single-facet and use putRef() then
-            FacetValue value = new FacetValue("dm4.workspaces.workspace").addRef(workspaceId);
-            facetsService.updateFacet(object, "dm4.workspaces.workspace_facet", value);
+            facetsService.updateFacet(object, "dm4.workspaces.workspace_facet",
+                new FacetValue("dm4.workspaces.workspace").putRef(workspaceId));
+            // Note: we are refering to an existing workspace. So we must put a topic *reference* (using putRef()).
             //
             // 2) store assignment property
             object.setProperty(PROP_WORKSPACE_ID, workspaceId, false);      // addToIndex=false

commit 4614196b0a35d13abd1c9119e7629a6eea3a8b68

Core: fix aggregation-one update code (#592).
Formerly the core throwed a ClassCastException when updating an aggregation-one facet 2 times in a row on the same object but with different values. A test to demonstrate the issue is included. Now this is fixed.

As a consequence creating types works again.
It was broken 4 days ago with commit [9d90a8bc].

The exception was:
{{{
Caused by: java.lang.ClassCastException: de.deepamehta.core.impl.AttachedTopic cannot be cast to de.deepamehta.core.RelatedTopic
	at de.deepamehta.core.impl.AttachedChildTopics.updateAggregationOne(AttachedChildTopics.java:380)
	at de.deepamehta.core.impl.AttachedChildTopics.updateChildTopics(AttachedChildTopics.java:243)
	at de.deepamehta.core.impl.AttachedDeepaMehtaObject.updateChildTopic(AttachedDeepaMehtaObject.java:194)
	at de.deepamehta.plugins.facets.FacetsPlugin.updateFacet(FacetsPlugin.java:139)
	at de.deepamehta.plugins.workspaces.WorkspacesPlugin._assignToWorkspace(WorkspacesPlugin.java:314)
}}}

Core API

A type model's `addAssocDef()` method now returns the type model (formerly void).
{{{
TopicTypeModel addAssocDef(...)
AssociationTypeModel addAssocDef(...)
}}}
This allows chaining several `addAssocDef()` calls when building composite types programmatically, e.g. in a test.

See #592.

modules/dm4-core/src/main/java/de/deepamehta/core/impl/AttachedChildTopics.java
@@ -1,8 +1,8 @@
 package de.deepamehta.core.impl;
 
+import de.deepamehta.core.Association;
 import de.deepamehta.core.AssociationDefinition;
 import de.deepamehta.core.ChildTopics;
-import de.deepamehta.core.DeepaMehtaObject;
 import de.deepamehta.core.RelatedTopic;
 import de.deepamehta.core.Topic;
 import de.deepamehta.core.model.ChildTopicsModel;
@@ -11,7 +11,7 @@ import de.deepamehta.core.model.SimpleValue;
 import de.deepamehta.core.model.TopicDeletionModel;
 import de.deepamehta.core.model.TopicModel;
 import de.deepamehta.core.model.TopicReferenceModel;
-import de.deepamehta.core.service.Directives;
+import de.deepamehta.core.model.TopicRoleModel;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -390,8 +390,7 @@ class AttachedChildTopics implements ChildTopics {
                 // == create assignment ==
             }
             // update DB
-            Topic topic = dms.valueStorage.associateReferencedChildTopic(parent.getModel(),
-                (TopicReferenceModel) newChildTopic, assocDef);
+            Topic topic = associateReferencedChildTopic((TopicReferenceModel) newChildTopic, assocDef);
             // update memory
             putInChildTopics(topic, assocDef);
         } else if (newChildTopic.getId() != -1) {
@@ -430,8 +429,7 @@ class AttachedChildTopics implements ChildTopics {
                 }
                 // == create assignment ==
                 // update DB
-                Topic topic = dms.valueStorage.associateReferencedChildTopic(parent.getModel(),
-                    (TopicReferenceModel) newChildTopic, assocDef);
+                Topic topic = associateReferencedChildTopic((TopicReferenceModel) newChildTopic, assocDef);
                 // update memory
                 addToChildTopics(topic, assocDef);
             } else if (childTopicId != -1) {
@@ -444,7 +442,54 @@ class AttachedChildTopics implements ChildTopics {
         }
     }
 
+    // --- ### TODO: avoid structural similar code, see ValueStorage
+
+    /**
+     * Creates an association between our parent object ("Parent" role) and the referenced topic ("Child" role).
+     * The association type is taken from the given association definition.
+     *
+     * @return  the resolved child topic.
+     */
+    RelatedTopic associateReferencedChildTopic(TopicReferenceModel childTopicRef, AssociationDefinition assocDef) {
+        if (childTopicRef.isReferenceById()) {
+            long childTopicId = childTopicRef.getId();
+            // Note: the resolved topic must be fetched including its composite value.
+            // It might be required at client-side. ### FIXME: had fetchComposite=true
+            Topic childTopic = dms.getTopic(childTopicId);
+            Association assoc = associateChildTopic(childTopicId, assocDef);
+            return createRelatedTopic(childTopic, assoc);
+        } else if (childTopicRef.isReferenceByUri()) {
+            String childTopicUri = childTopicRef.getUri();
+            // Note: the resolved topic must be fetched including its composite value.
+            // It might be required at client-side. ### FIXME: had fetchComposite=true
+            Topic childTopic = dms.getTopic("uri", new SimpleValue(childTopicUri));
+            Association assoc = associateChildTopic(childTopicUri, assocDef);
+            return createRelatedTopic(childTopic, assoc);
+        } else {
+            throw new RuntimeException("Invalid topic reference (" + childTopicRef + ")");
+        }
+    }
+
+    private Association associateChildTopic(long childTopicId, AssociationDefinition assocDef) {
+        return associateChildTopic(new TopicRoleModel(childTopicId, "dm4.core.child"), assocDef);
+    }
+
+    private Association associateChildTopic(String childTopicUri, AssociationDefinition assocDef) {
+        return associateChildTopic(new TopicRoleModel(childTopicUri, "dm4.core.child"), assocDef);
+    }
+
     // ---
+
+    private Association associateChildTopic(TopicRoleModel child, AssociationDefinition assocDef) {
+        return dms.createAssociation(assocDef.getInstanceLevelAssocTypeUri(),
+            parent.getModel().createRoleModel("dm4.core.parent"), child);
+    }
+
+    private RelatedTopic createRelatedTopic(Topic topic, Association assoc) {
+        return new AttachedRelatedTopic(new RelatedTopicModel(topic.getModel(), assoc.getModel()), dms);
+    }
+
+    // --- ### end TODO
 
     private void updateChildTopicOne(TopicModel newChildTopic, AssociationDefinition assocDef) {
         AttachedTopic childTopic = _getTopic(assocDef.getChildTypeUri(), null);

modules/dm4-core/src/main/java/de/deepamehta/core/impl/ValueStorage.java
@@ -166,21 +166,21 @@ class ValueStorage {
      * Creates an association between the given parent object ("Parent" role) and the referenced topic ("Child" role).
      * The association type is taken from the given association definition.
      *
-     * @return  the resolved child topic, including its composite values.
+     * @return  the resolved child topic.
      */
-    Topic associateReferencedChildTopic(DeepaMehtaObjectModel parent, TopicReferenceModel childTopicRef,
-                                                                      AssociationDefinition assocDef) {
+    private Topic associateReferencedChildTopic(DeepaMehtaObjectModel parent, TopicReferenceModel childTopicRef,
+                                                                              AssociationDefinition assocDef) {
         if (childTopicRef.isReferenceById()) {
             long childTopicId = childTopicRef.getId();
             associateChildTopic(parent, childTopicId, assocDef);
             // Note: the resolved topic must be fetched including its composite value.
-            // It might be required at client-side.
+            // It might be required at client-side. ### TODO
             return dms.getTopic(childTopicId);                          // ### FIXME: had fetchComposite=true
         } else if (childTopicRef.isReferenceByUri()) {
             String childTopicUri = childTopicRef.getUri();
             associateChildTopic(parent, childTopicUri, assocDef);
             // Note: the resolved topic must be fetched including its composite value.
-            // It might be required at client-side.
+            // It might be required at client-side. ### TODO
             return dms.getTopic("uri", new SimpleValue(childTopicUri)); // ### FIXME: had fetchComposite=true
         } else {
             throw new RuntimeException("Invalid topic reference (" + childTopicRef + ")");

modules/dm4-core/src/main/java/de/deepamehta/core/model/AssociationTypeModel.java
@@ -37,6 +37,13 @@ public class AssociationTypeModel extends TypeModel {
     // -------------------------------------------------------------------------------------------------- Public Methods
 
     @Override
+    public AssociationTypeModel addAssocDef(AssociationDefinitionModel assocDef) {
+        return (AssociationTypeModel) super.addAssocDef(assocDef);
+    }
+
+    // ---
+
+    @Override
     public String toString() {
         return "association type (" + super.toString() + ")";
     }

modules/dm4-core/src/main/java/de/deepamehta/core/model/TopicTypeModel.java
@@ -41,6 +41,13 @@ public class TopicTypeModel extends TypeModel {
     // -------------------------------------------------------------------------------------------------- Public Methods
 
     @Override
+    public TopicTypeModel addAssocDef(AssociationDefinitionModel assocDef) {
+        return (TopicTypeModel) super.addAssocDef(assocDef);
+    }
+
+    // ---
+
+    @Override
     public String toString() {
         return "topic type (" + super.toString() + ")";
     }

modules/dm4-core/src/main/java/de/deepamehta/core/model/TypeModel.java
@@ -102,7 +102,7 @@ public abstract class TypeModel extends TopicModel {
         return assocDef;
     }
 
-    public void addAssocDef(AssociationDefinitionModel assocDef) {
+    public TypeModel addAssocDef(AssociationDefinitionModel assocDef) {
         // error check ### FIXME: drop this check or provide proper feedback to the type editor user
         if (!getDataTypeUri().equals("dm4.core.composite")) {
             throw new RuntimeException("Association definitions can only be added to composite topic types. " +
@@ -117,6 +117,8 @@ public abstract class TypeModel extends TopicModel {
         }
         //
         updateAssocDef(assocDef);
+        //
+        return this;
     }
 
     public void updateAssocDef(AssociationDefinitionModel assocDef) {

modules/dm4-help/src/main/resources/web/script/plugin.js
@@ -15,7 +15,7 @@ dm4c.add_plugin("de.deepamehta.help", function() {
                         '<div class="field-label">Version</div>' +
                         '<div>${project.version}</div>' +
                         '<div class="field-label">Release Date</div>' +
-                        '<div>Jan 9, 2015</div>' +
+                        '<div>Jan 10, 2015</div>' +
                         '<div class="field-label">Copyright</div>' +
                         '<div>2000-2015 Jörg Richter et al.</div>' +
                         '<div class="field-label">License</div>' +

modules/dm4-test/src/test/java/de/deepamehta/core/impl/CoreServiceTest.java
@@ -8,12 +8,15 @@ import de.deepamehta.core.RelatedTopic;
 import de.deepamehta.core.Topic;
 import de.deepamehta.core.TopicType;
 import de.deepamehta.core.Type;
+import de.deepamehta.core.model.AssociationDefinitionModel;
 import de.deepamehta.core.model.AssociationModel;
 import de.deepamehta.core.model.AssociationRoleModel;
 import de.deepamehta.core.model.ChildTopicsModel;
 import de.deepamehta.core.model.SimpleValue;
 import de.deepamehta.core.model.TopicModel;
+import de.deepamehta.core.model.TopicReferenceModel;
 import de.deepamehta.core.model.TopicRoleModel;
+import de.deepamehta.core.model.TopicTypeModel;
 import de.deepamehta.core.service.ResultList;
 import de.deepamehta.core.storage.spi.DeepaMehtaTransaction;
 
@@ -433,6 +436,137 @@ public class CoreServiceTest extends CoreServiceTestEnvironment {
         topics = getTopicInstancesByTraversal(type);
         assertEquals(4, topics.getSize());
         // ### Note: the Lucene index update was already visible within the transaction!
+    }
+
+    // ---
+
+    @Test
+    public void updateAggregationOne() {
+        DeepaMehtaTransaction tx = dms.beginTx();
+        Topic comp1, item1, item2;
+        try {
+            // 1) define composite type
+            // child types
+            dms.createTopicType(new TopicTypeModel("dm4.test.name", "Name", "dm4.core.text"));
+            dms.createTopicType(new TopicTypeModel("dm4.test.item", "Item", "dm4.core.text"));
+            // parent type
+            dms.createTopicType(new TopicTypeModel("dm4.test.composite", "Composite", "dm4.core.composite")
+                .addAssocDef(new AssociationDefinitionModel("dm4.core.composition_def",
+                    "dm4.test.composite", "dm4.test.name", "dm4.core.one", "dm4.core.one"
+                ))
+                .addAssocDef(new AssociationDefinitionModel("dm4.core.aggregation_def",
+                    "dm4.test.composite", "dm4.test.item", "dm4.core.many", "dm4.core.one"
+                ))
+            );
+            // 2) create example child instances
+            item1 = dms.createTopic(new TopicModel("dm4.test.item", new SimpleValue("Item 1")));
+            item2 = dms.createTopic(new TopicModel("dm4.test.item", new SimpleValue("Item 2")));
+            // 3) create composite instance
+            comp1 = dms.createTopic(new TopicModel("dm4.test.composite", new ChildTopicsModel()
+                .put("dm4.test.name", "Composite 1")
+                // ### .putRef("dm4.test.item", item1.getId())
+            ));
+            tx.success();
+        } finally {
+            tx.finish();
+        }
+        // check memory
+        assertEquals("Composite 1", comp1.getChildTopics().getString("dm4.test.name"));
+        assertFalse(                comp1.getChildTopics().has("dm4.test.item"));
+        comp1.loadChildTopics();
+        assertFalse(                comp1.getChildTopics().has("dm4.test.item"));
+        assertEquals(2, dms.getTopics("dm4.test.item", 0).getSize());
+        //
+        // update and check again
+        tx = dms.beginTx();
+        try {
+            comp1.update(new TopicModel(comp1.getId(), new ChildTopicsModel()
+                .putRef("dm4.test.item", item2.getId())
+            ));
+            tx.success();
+        } finally {
+            tx.finish();
+        }
+        //
+        assertEquals("Composite 1", comp1.getChildTopics().getString("dm4.test.name"));
+        assertTrue(                 comp1.getChildTopics().has("dm4.test.item"));
+        assertEquals("Item 2",      comp1.getChildTopics().getString("dm4.test.item"));
+        assertEquals(item2.getId(), comp1.getChildTopics().getTopic("dm4.test.item").getId());
+        assertEquals(2, dms.getTopics("dm4.test.item", 0).getSize());
+        //
+        // update and check again
+        tx = dms.beginTx();
+        try {
+            comp1.update(new TopicModel(comp1.getId(), new ChildTopicsModel()
+                .putRef("dm4.test.item", item1.getId())
+            ));
+            tx.success();
+        } finally {
+            tx.finish();
+        }
+        //
+        assertEquals("Composite 1", comp1.getChildTopics().getString("dm4.test.name"));
+        assertTrue(                 comp1.getChildTopics().has("dm4.test.item"));
+        assertEquals("Item 1",      comp1.getChildTopics().getString("dm4.test.item"));
+        assertEquals(item1.getId(), comp1.getChildTopics().getTopic("dm4.test.item").getId());
+        assertEquals(2, dms.getTopics("dm4.test.item", 0).getSize());
+    }
+
+    @Test
+    public void updateAggregationOneFacet() {
+        DeepaMehtaTransaction tx = dms.beginTx();
+        Topic name, item1, item2;
+        try {
+            // 1) define facet
+            dms.createTopicType(new TopicTypeModel("dm4.test.item", "Item", "dm4.core.text"));
+            dms.createTopicType(new TopicTypeModel("dm4.test.item_facet", "Item Facet", "dm4.core.composite")
+                .addAssocDef(new AssociationDefinitionModel("dm4.core.aggregation_def",
+                    "dm4.test.item_facet", "dm4.test.item", "dm4.core.many", "dm4.core.one"
+                ))
+            );
+            // 2) create example facet values
+            item1 = dms.createTopic(new TopicModel("dm4.test.item", new SimpleValue("Item 1")));
+            item2 = dms.createTopic(new TopicModel("dm4.test.item", new SimpleValue("Item 2")));
+            // 3) define simple type + instance
+            dms.createTopicType(new TopicTypeModel("dm4.test.name", "Name", "dm4.core.text"));
+            name = dms.createTopic(new TopicModel("dm4.test.name", new SimpleValue("Name 1")));
+            //
+            tx.success();
+        } finally {
+            tx.finish();
+        }
+        //
+        AssociationDefinition assocDef = dms.getTopicType("dm4.test.item_facet").getAssocDef("dm4.test.item");
+        //
+        // update facet
+        tx = dms.beginTx();
+        try {
+            name.updateChildTopic(new TopicReferenceModel(item1.getId()), assocDef);
+            tx.success();
+        } finally {
+            tx.finish();
+        }
+        //
+        assertTrue(                 name.getChildTopics().has("dm4.test.item"));
+        Topic facetValue = (Topic)  name.getChildTopics().get("dm4.test.item");
+        assertEquals("Item 1",      facetValue.getSimpleValue().toString());
+        assertEquals(item1.getId(), facetValue.getId());
+        assertEquals(2, dms.getTopics("dm4.test.item", 0).getSize());
+        //
+        // update facet again
+        tx = dms.beginTx();
+        try {
+            name.updateChildTopic(new TopicReferenceModel(item2.getId()), assocDef);
+            tx.success();
+        } finally {
+            tx.finish();
+        }
+        //
+        assertTrue(                 name.getChildTopics().has("dm4.test.item"));
+        facetValue = (Topic)        name.getChildTopics().get("dm4.test.item");
+        assertEquals("Item 2",      facetValue.getSimpleValue().toString());
+        assertEquals(item2.getId(), facetValue.getId());
+        assertEquals(2, dms.getTopics("dm4.test.item", 0).getSize());
     }
 
     // ---