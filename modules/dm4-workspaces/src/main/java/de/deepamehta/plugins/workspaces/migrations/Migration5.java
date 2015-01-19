42b11a946a823ed7d04fad5c2fe6544b7c52537b
Jörg Richter <jri@deepamehta.de>
2015-01-19 06:39:59 +0100

    Add migration for converting workspaces (#592).
    When updating an installation to DM 4.5: all workspaces existing in the pre-4.5 installation get sharing mode "Public". That reflects the pre-4.5 access control semantics. You can change a workspace's sharing mode interactively in the Webclient (or programmatically via API).
    
    3 workspace migrations are consolidated into one.
    
    See #592.

--- modules/dm4-workspaces/src/main/java/de/deepamehta/plugins/workspaces/migrations/Migration5.java
+++ modules/dm4-workspaces/src/main/java/de/deepamehta/plugins/workspaces/migrations/Migration5.java
@@ -1,12 +1,14 @@
 package de.deepamehta.plugins.workspaces.migrations;
 
+import de.deepamehta.core.Topic;
+import de.deepamehta.core.model.ChildTopicsModel;
 import de.deepamehta.core.model.SimpleValue;
+import de.deepamehta.core.model.TopicModel;
 import de.deepamehta.core.service.Migration;
 
 
 
 /**
- * Corrects URI of the "DeepaMehta" workspace: "de." -> "dm4."
  * Runs only in UPDATE mode.
  * <p>
  * Part of DM 4.5
@@ -15,6 +17,26 @@ public class Migration5 extends Migration {
 
     @Override
     public void run() {
+        //
+        // 1) Changes cardinality of the workspace facet to "one".
+        //
+        dms.getTopicType("dm4.workspaces.workspace_facet").getAssocDef("dm4.workspaces.workspace")
+            .setChildCardinalityUri("dm4.core.one");
+        //
+        // 2) Corrects URI of the "DeepaMehta" workspace: "de." -> "dm4."
+        //
         dms.getTopic("uri", new SimpleValue("de.workspaces.deepamehta")).setUri("dm4.workspaces.deepamehta");
+        //
+        // 3) Sets the sharing mode of all workspaces to "Public".
+        //
+        for (Topic workspace : dms.getTopics("dm4.workspaces.workspace", 0)) {
+            workspace.update(new TopicModel(null, new ChildTopicsModel()
+                .putRef("dm4.workspaces.sharing_mode", "dm4.workspaces.public")
+            ));
+            // Note: instead of calling update(...) on the entire topic object we could update the child selectively:
+            //     workspace.getChildTopics().setRef("dm4.workspaces.sharing_mode", "dm4.workspaces.public")
+            // This would be much more concise. However in this case the topic will loose its label.
+            // ### TODO: fix that error in the labeling mechanism.
+        }
     }
 }
