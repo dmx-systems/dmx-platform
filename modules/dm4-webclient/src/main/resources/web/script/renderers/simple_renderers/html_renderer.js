f2c07c277191c4a020cee491effcd5e73c5cef9d
Jörg Richter <jri@deepamehta.de>
2015-05-23 18:39:49 +0200

    Webclient: HTML childs with card. "many" (#792).
    The detail panel's form rendering works also if the type definition of the selected topic/association includes child topics of data type "HTML" and cardinality "Many".
    
    Thank you very much, Juergen, for reporting!
    
    See #792.

--- modules/dm4-webclient/src/main/resources/web/script/renderers/simple_renderers/html_renderer.js
+++ modules/dm4-webclient/src/main/resources/web/script/renderers/simple_renderers/html_renderer.js
@@ -6,17 +6,17 @@ dm4c.add_simple_renderer("dm4.webclient.html_renderer", {
     },
 
     render_form: function(page_model, parent_element) {
-        parent_element.append($("<textarea>")
-            .attr({id: "field_" + page_model.uri, rows: page_model.input_field_rows})
+        var textarea = $("<textarea>")
+            .attr("rows", page_model.input_field_rows)
             .text(page_model.value)
-        )
-        CKEDITOR.replace("field_" + page_model.uri, {
+        parent_element.append(textarea)
+        var editor = CKEDITOR.replace(textarea.get(0), {
             contentsCss: "/de.deepamehta.webclient/css/ckeditor-contents.css",
             autoGrow_onStartup: true
         })
         //
         return function() {
-            return CKEDITOR.instances["field_" + page_model.uri].getData()
+            return editor.getData()
         }
     }
 })
