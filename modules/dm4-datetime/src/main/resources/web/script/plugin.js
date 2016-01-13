78a9ca13681da8011e0d981d02ec00a56a512e77
Jörg Richter <jri@deepamehta.de>
2016-01-13 18:50:08 +0100

    Datetime: combobox shows month names (#901).
    "Jan", "Feb", "Mar", ...
    
    Note: at the moment this applies only to comboboxes, that is when the detail panel is in form mode.
    Does not yet apply for detail panel info mode, or canvas rendering.
    
    See #901.

---
+++ modules/dm4-datetime/src/main/resources/web/script/plugin.js
@@ -0,0 +1,17 @@
+dm4c.add_plugin("de.deepamehta.datetime", function() {
+
+    var month_names = [undefined, "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+    // === Webclient Listeners ===
+
+    dm4c.add_listener("option_topics", function(page_model) {
+        var topic_type_uri = page_model.object_type.uri
+        if (topic_type_uri == "dm4.datetime.month") {
+            var month_topics = dm4c.restc.get_topics(topic_type_uri, false, true).items     // include_childs=false,
+            month_topics.forEach(function(month_topic) {                                    //           sort=true
+                month_topic.value = month_names[month_topic.value]
+            })
+            return month_topics
+        }
+    })
+})
