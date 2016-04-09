122824d37a49292fb48311b2a871ae0a33df8da2
Jörg Richter <jri@deepamehta.de>
2016-04-09 04:38:48 +0200

    CKEditor: adjust Styles menu (#700).
    Now we have these block styles:
    * Paragraph
    * Heading 1-4
    * Code Block
    * Block Quote
    
    And these inline styles:
    * Code
    * Marker
    
    Thanks to Malte for the reminder!
    Tell me if the styles suit your needs.
    
    See #700.

--- modules/dm4-webclient/src/main/resources/web/script/renderers/simple_renderers/html_renderer.js
+++ modules/dm4-webclient/src/main/resources/web/script/renderers/simple_renderers/html_renderer.js
@@ -13,7 +13,20 @@ dm4c.add_simple_renderer("dm4.webclient.html_renderer", {
         parent_element.append(textarea)
         var editor = CKEDITOR.replace(textarea.get(0), {
             contentsCss: "/de.deepamehta.webclient/css/ckeditor-contents.css",
-            autoGrow_onStartup: true
+            autoGrow_onStartup: true,
+            stylesSet: [
+                /* Block Styles */
+                {name: "Paragraph",   element: "p"},
+                {name: "Heading 1",   element: "h1"},
+                {name: "Heading 2",   element: "h2"},
+                {name: "Heading 3",   element: "h3"},
+                {name: "Heading 4",   element: "h4"},
+                {name: "Code Block",  element: "pre",  attributes: {class: "code-block"}},
+                {name: "Block Quote", element: "div",  attributes: {class: "blockquote"}},
+                /* Inline Styles */
+                {name: "Code",        element: "code"},
+                {name: "Marker",      element: "span", attributes: {class: "marker"}}
+            ]
         })
         //
         return function() {
