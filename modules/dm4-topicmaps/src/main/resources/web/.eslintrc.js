2c862d9e3a5ab0f6854818b0c30311b13ec8f19d
Jörg Richter <jri@deepamehta.de>
2017-04-18 20:18:54 +0200

    ESLint: DM plugins use Standard style too (#1073)

--- modules/dm4-topicmaps/src/main/resources/web/.eslintrc.js
+++ modules/dm4-topicmaps/src/main/resources/web/.eslintrc.js
@@ -20,11 +20,5 @@ module.exports = {
   ],
   // add your custom rules here
   'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    'one-var': 0,
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'brace-style': [2, 'stroustrup', { 'allowSingleLine': true }]
   }
 }
