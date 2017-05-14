f3653080ca04c81eaabd7638e39add181092ad04
Jörg Richter <jri@deepamehta.de>
2017-05-14 23:47:37 +0200

    Add Element UI `Table` and `TableColumn` (#1073)

--- modules/dm4-webclient/src/main/resources/web/src/element-ui.js
+++ modules/dm4-webclient/src/main/resources/web/src/element-ui.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, Input, Select, Option, Dialog } from 'element-ui'
+import { Button, Input, Select, Option, Dialog, Table, TableColumn } from 'element-ui'
 import locale from 'element-ui/lib/locale'
 
 // set locale
@@ -11,3 +11,5 @@ Vue.use(Input)
 Vue.use(Select)
 Vue.use(Option)
 Vue.use(Dialog)
+Vue.use(Table)
+Vue.use(TableColumn)
