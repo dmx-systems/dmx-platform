b0b76977ccf6eb5206ef3df91af2930a7b013c94
Jörg Richter <jri@deepamehta.de>
2017-10-08 19:18:27 +0200

    Render assoc colors (#1073)
    See #1073

--- modules/dm4-topicmaps/src/main/js/main.js
+++ modules/dm4-topicmaps/src/main/js/main.js
@@ -1,20 +1,41 @@
-export default (store) => ({
+export default (store) => {
 
-  storeModule: {
-    name: 'topicmaps',
-    module: require('./topicmaps')
-  },
-
-  components: {
-    webclient: require('dm5-topicmap-panel'),
-    toolbar: require('./components/TopicmapSelect')
-  },
-
-  extraMenuItems: [{
-    uri: 'dm4.topicmaps.topicmap',
-    label: 'Topicmap',
-    create: name => {
-      store.dispatch('createTopicmap', name)
+  // TODO: declarative store watchers?
+  store.watch(
+    state => state.typeCache.assocTypes,
+    assocTypes => {
+      store.dispatch('syncStyles', assocTypeColors())
     }
-  }]
-})
+  )
+
+  return {
+
+    storeModule: {
+      name: 'topicmaps',
+      module: require('./topicmaps')
+    },
+
+    components: {
+      webclient: require('dm5-topicmap-panel'),
+      toolbar: require('./components/TopicmapSelect')
+    },
+
+    extraMenuItems: [{
+      uri: 'dm4.topicmaps.topicmap',
+      label: 'Topicmap',
+      create: name => {
+        store.dispatch('createTopicmap', name)
+      }
+    }]
+  }
+
+  function assocTypeColors() {
+    return Object.values(store.state.typeCache.assocTypes).reduce((colors, assocType) => {
+      const color = assocType.getColor()
+      if (color) {
+        colors[assocType.uri] = color
+      }
+      return colors
+    }, {})
+  }
+}
