7c3c638b447e35e53e85d43bf678631495c62ed8
Jörg Richter <jri@deepamehta.de>
2017-10-12 03:06:38 +0200

    Topic type editor (#1073)
    See #1073

--- modules/dm4-typeeditor/src/main/js/components/TypeRenderer.vue
+++ modules/dm4-typeeditor/src/main/js/components/TypeRenderer.vue
@@ -1,10 +1,48 @@
 <template>
-  <h1>:-)</h1>
+  <div>
+    <field-renderer :object="object" :mode="mode"></field-renderer>
+    <!-- Data Type -->
+    <div class="field-label">Data Type</div>
+    <div v-if="infoMode">{{dataType.value}}</div>
+    <el-select v-else v-model="type.dataType" size="small">
+      <el-option v-for="dataType in dataTypes" :label="dataType.value" :value="dataType.uri" :key="dataType.uri">
+      </el-option>
+    </el-select>
+  </div>
 </template>
 
 <script>
+import dm5 from 'dm5'
+
 export default {
-  // TODO
+
+  props: [
+    'object',   // the type to render (a dm5.Topic)
+    'mode'      // 'info' or 'form'
+  ],
+
+  computed: {
+
+    type () {
+      return this.object.asType()
+    },
+
+    dataType () {
+      return this.type.getDataType()
+    },
+
+    dataTypes () {
+      return this.$store.state.typeCache.dataTypes
+    }
+  },
+
+  mixins: [
+    require('modules-nodejs/dm5-detail-panel/src/components/mixins/infoMode').default
+  ],
+
+  components: {
+    'field-renderer': require('modules-nodejs/dm5-detail-panel/src/components/FieldRenderer')
+  }
 }
 </script>
 
