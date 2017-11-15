ce98a49d6e6be4230ba8c3e624fff006efaefa60
Jörg Richter <jri@deepamehta.de>
2017-11-15 13:35:40 +0100

    `Select` widget for Workspace Sharing Mode (#1073)
    See #1073

--- modules/dm4-workspaces/src/main/java/de/deepamehta/workspaces/migrations/Migration3.java
+++ modules/dm4-workspaces/src/main/java/de/deepamehta/workspaces/migrations/Migration3.java
@@ -1,12 +1,11 @@
 package de.deepamehta.workspaces.migrations;
 
-import de.deepamehta.core.model.AssociationDefinitionModel;
 import de.deepamehta.core.service.Migration;
 
 
 
 /**
- * Adds child type "Sharing Mode" to the "Workspace" topic type.
+ * Adds "Sharing Mode" to "Workspace".
  * Runs ALWAYS.
  * <p>
  * Part of DM 4.5
@@ -15,9 +14,12 @@ public class Migration3 extends Migration {
 
     @Override
     public void run() {
-        dm4.getTopicType("dm4.workspaces.workspace").addAssocDef(
-            mf.newAssociationDefinitionModel("dm4.core.aggregation_def",
-                "dm4.workspaces.workspace", "dm4.workspaces.sharing_mode", "dm4.core.many", "dm4.core.one")
-        );
+        dm4.getTopicType("dm4.workspaces.workspace").addAssocDef(mf.newAssociationDefinitionModel(
+            "dm4.core.aggregation_def",
+            "dm4.workspaces.workspace", "dm4.workspaces.sharing_mode", "dm4.core.many", "dm4.core.one",
+            mf.newViewConfigurationModel().setConfigValueRef(
+                "dm4.webclient.view_config", "dm4.webclient.widget", "dm4.webclient.select"
+            )
+        ));
     }
 }
