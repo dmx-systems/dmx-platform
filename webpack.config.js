7354b561ce2a1cb767265b0f28a39b73b742f5a6
Jörg Richter <jri@deepamehta.de>
2018-03-04 15:20:45 +0100

    Upgrade to `babel-preset-env` (#1073)
    See #1073

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,54 @@
 const path = require('path')
 
-module.exports = (env = {}) => {
-
-  const webpackConfig = {
-    entry: './modules/dm4-webclient/src/main/js/main.js',
-    output: {
-      path: path.resolve(__dirname, 'modules/dm4-webclient/src/main/resources/web/dist'),
-      publicPath: 'dist/'
-    },
-    resolve: {
-      extensions: [".js", ".vue"],
-      alias: {
-        'modules': path.resolve(__dirname, 'modules')     // needed by plugin-manager.js
-      }
-    },
-    module: {
-      rules: [
-        {
-          test: /\.vue$/,
-          loader: 'vue-loader'
-        },
-        {
-          test: /\.js$/,
-          loader: 'babel-loader',
-          exclude: /node_modules\/(?!dm5)/
-          // Note: at the moment the dm5 modules are distributed not in pre-compiled form.
-          // For the production build we must include them in babel processing as UglifyJs accpets no ES6.
-          // Note: regex x(?!y) matches x only if x is not followed by y. Only x is part of the match results.
-          // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp
-          // TODO: distribute the dm5 modules in pre-compiled form.
-        },
-        {
-          test: /\.css$/,
-          loader: ['style-loader', 'css-loader']
-        },
-        {
-          test: /\.(png|jpg|jpeg|gif|eot|ttf|woff|woff2|svg|svgz)(\?.+)?$/,
-          loader: 'file-loader'
-        }
-      ]
+module.exports = {
+  entry: './modules/dm4-webclient/src/main/js/main.js',
+  output: {
+    path: path.resolve(__dirname, 'modules/dm4-webclient/src/main/resources/web/dist'),
+    publicPath: 'dist/'
+  },
+  resolve: {
+    extensions: [".js", ".vue"],
+    alias: {
+      'modules': path.resolve(__dirname, 'modules')     // needed by plugin-manager.js
     }
-  }
-
-  if (env.dev) {
-    webpackConfig.devServer = {
-      port: 8082,
-      proxy: {
-        '/': {
-          target: 'http://localhost:8080',
-          bypass: req => {
-            if (req.url === '/') {
-              return 'modules/dm4-webclient/src/main/resources/web/'
-            }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|eot|ttf|woff|woff2|svg|svgz)(\?.+)?$/,
+        loader: 'file-loader'
+      }
+    ]
+  },
+  devServer: {
+    port: 8082,
+    proxy: {
+      '/': {
+        target: 'http://localhost:8080',
+        bypass: req => {
+          if (req.url === '/') {
+            return 'modules/dm4-webclient/src/main/resources/web/'
           }
         }
-      },
-      noInfo: true,
-      open: true
-    }
+      }
+    },
+    noInfo: true,
+    open: true
+  },
+  performance: {
+    hints: false
   }
-
-  return webpackConfig
 }
