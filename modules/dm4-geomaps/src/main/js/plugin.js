373799d9581583b7f9331aa7ac637e90ccd82ea4
Jörg Richter <jri@deepamehta.de>
2018-03-28 16:39:40 +0200

    Load Topicmap Renderers asynchronously (#1073)
    See #1073

--- modules/dm4-geomaps/src/main/js/plugin.js
+++ modules/dm4-geomaps/src/main/js/plugin.js
@@ -4,6 +4,6 @@ export default {
     uri: 'dm4.geomaps.geomap_renderer',
     name: "Geomap",
     storeModule: require('./geomaps').default,  // TODO
-    comp: require('./components/dm5-geomap-renderer').default
+    comp: () => import('./components/dm5-geomap-renderer' /* webpackChunkName: "leaflet" */)
   }
 }
