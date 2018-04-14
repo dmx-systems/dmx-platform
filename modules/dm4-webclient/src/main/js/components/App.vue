dcea6494b53c76c92c969638bb0353fd9ec4841e
Jörg Richter <jri@deepamehta.de>
2018-04-14 12:29:29 +0200

    Update to Element UI 2.3.4, fixes border-cart CSS (#1073)
    See #1073

--- modules/dm4-webclient/src/main/js/components/App.vue
+++ modules/dm4-webclient/src/main/js/components/App.vue
@@ -38,7 +38,6 @@
   --line-height: 1.6em;
   --paragraph-spacing: 0.6em;
   --highlight-color: #409eff;         /* see --color-primary */
-  --color-text-disabled: #c0c4cc;     /* see --color-text-placeholder */
   --background-color: #f5f7fa;        /* detail panel background, map topics background, see --background-color-base */
   --background-color-darker: #ebeef5;
   --color-danger: #f56c6c;            /* see --color-danger */
