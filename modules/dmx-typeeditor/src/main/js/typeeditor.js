b91068ed7ac9cb6b50da8914e4d4d5d0585d38df
Jörg Richter <jri@deepamehta.de>
2018-08-15 11:59:57 +0200

    Fix: create assoc types works (closes #25)

--- modules/dmx-typeeditor/src/main/js/typeeditor.js
+++ modules/dmx-typeeditor/src/main/js/typeeditor.js
@@ -1,28 +1,22 @@
 import dm5 from 'dm5'
 
-function defaultType (name) {
-  return {
-    // Note: no type "uri" is set here. A new type gets its default URI at server-side.
-    // Also the "typeUri" is provided at server-side (see ModelFactoryImpl).
-    value: name,
-    dataTypeUri: 'dmx.core.text',
-    indexModeUris: ['dmx.core.key', 'dmx.core.fulltext'],
-    viewConfigTopics: [{
-      typeUri: 'dmx.webclient.view_config',
-      childs: {
-        'dmx.webclient.show_in_create_menu': true
-      }
-    }]
-  }
-}
-
 const actions = {
+
   createTopicType ({dispatch}, {name, pos}) {
     console.log('Creating topic type', name)
-    dm5.restClient.createTopicType(defaultType(name)).then(topicType => {
+    dm5.restClient.createTopicType(defaultTopicType(name)).then(topicType => {
       console.log('Created', topicType)
       dispatch('putTopicType', topicType)
       dispatch('revealTopic', {topic: topicType, pos, select: true})
+    })
+  },
+
+  createAssocType ({dispatch}, {name, pos}) {
+    console.log('Creating assoc type', name)
+    dm5.restClient.createAssocType(defaultAssocType(name)).then(assocType => {
+      console.log('Created', assocType)
+      dispatch('putAssocType', assocType)
+      dispatch('revealTopic', {topic: assocType, pos, select: true})
     })
   }
 }
@@ -30,3 +24,25 @@ const actions = {
 export default {
   actions
 }
+
+const defaultTopicType = name => ({
+  // Note: no type "uri" is set here. A new type gets its default URI at server-side.
+  // Also the "typeUri" is provided at server-side (see ModelFactoryImpl).
+  value: name,
+  dataTypeUri: 'dmx.core.text',
+  indexModeUris: ['dmx.core.key', 'dmx.core.fulltext'],
+  viewConfigTopics: [{
+    typeUri: 'dmx.webclient.view_config',
+    childs: {
+      'dmx.webclient.show_in_create_menu': true
+    }
+  }]
+})
+
+const defaultAssocType = name => ({
+  // Note: no type "uri" is set here. A new type gets its default URI at server-side.
+  // Also the "typeUri" is provided at server-side (see ModelFactoryImpl).
+  value: name,
+  dataTypeUri: 'dmx.core.text',
+  indexModeUris: ['dmx.core.key', 'dmx.core.fulltext']
+})
