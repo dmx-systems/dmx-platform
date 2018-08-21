c138c868f72c12272ec4743a4084b0980cfc1714
Jörg Richter <jri@deepamehta.de>
2018-08-21 16:57:09 +0200

    dmx-search: adapt to search widget (handle events) (#26)

--- modules/dmx-typesearch/src/main/js/plugin.js
+++ modules/dmx-typesearch/src/main/js/plugin.js
@@ -1,21 +1,54 @@
-export default store => ({
+import dm5 from 'dm5'
 
-  storeModule: {
-    name: 'search',
-    module: require('./search').default
-  },
+export default store => {
+  return {
 
-  components: [
-    {
-      comp: require('dm5-search-widget').default,
-      mount: 'webclient',
-      props: {
-        visible:        state => state.search.visible,
-        pos:            state => state.search.pos,
-        options:        state => state.search.options,
-        extraMenuItems: state => state.search.extraMenuItems,
-        menuTopicTypes: (_, getters) => getters && getters.menuTopicTypes // TODO: why is getters undefined on 1st call?
+    storeModule: {
+      name: 'search',
+      module: require('./search').default
+    },
+
+    components: [
+      {
+        comp: require('dm5-search-widget').default,
+        mount: 'webclient',
+        props: {
+          visible:        state => state.search.visible,
+          pos:            state => state.search.pos,
+          extraMenuItems: state => state.search.extraMenuItems,
+          menuTopicTypes: (_, getters) => getters && getters.menuTopicTypes // TODO: why is getters undefined on 1st call?
+        },
+        listeners: {
+          'topic-reveal': revealTopic,
+          'topic-create': createTopic,
+          'extra-create': createExtra,
+          close: _ => store.dispatch('closeSearchWidget')
+        }
       }
-    }
-  ]
-})
+    ]
+  }
+
+  function revealTopic (topic) {
+    const state = store.state.search
+    store.dispatch('revealTopic', {
+      topic,
+      pos: state.pos.model,
+      select: !(state.options && state.options.noSelect)
+    })
+    state.options && state.options.topicHandler && state.options.topicHandler(topic)
+  }
+
+  function createTopic ({topicType, value}) {
+    const topicModel = topicType.newTopicModel(value)
+    // console.log('createTopic', topicModel)
+    dm5.restClient.createTopic(topicModel).then(topic => {
+      console.log('Created', topic)
+      revealTopic(topic)
+      store.dispatch('_processDirectives', topic.directives)
+    })
+  }
+
+  function createExtra ({extraItem, value, optionsData}) {
+    extraItem.create(value, optionsData, store.state.search.pos.model)
+  }
+}
