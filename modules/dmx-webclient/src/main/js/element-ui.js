dcca7b494dde213c4a32865dd23e1a8096fb6f51
Jörg Richter <jri@deepamehta.de>
2018-10-13 01:18:58 +0200

    Add color picker (#49)

--- modules/dmx-webclient/src/main/js/element-ui.js
+++ modules/dmx-webclient/src/main/js/element-ui.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import {
-  Button, Input, Select, Option, OptionGroup, Checkbox, Radio, RadioGroup, Dialog, Tabs, TabPane, Loading, Notification
+  Button, Input, Select, Option, OptionGroup, Checkbox, Radio, RadioGroup, Dialog, Tabs, TabPane, ColorPicker,
+  Loading, Notification
 } from 'element-ui'
 import locale from 'element-ui/lib/locale'
 
@@ -24,6 +25,7 @@ Vue.use(RadioGroup)
 Vue.use(Dialog)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(ColorPicker)
 
 Vue.use(Loading.directive)
 
