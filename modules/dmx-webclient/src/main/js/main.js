0bdbdf73ae96c030ba55ae5d351782eecf1f7efe
Jörg Richter <jri@deepamehta.de>
2018-10-21 16:40:31 +0200

    package.json: add `vue-font-awesome-search` dependency

--- modules/dmx-webclient/src/main/js/main.js
+++ modules/dmx-webclient/src/main/js/main.js
@@ -9,7 +9,7 @@ import 'font-awesome/css/font-awesome.css'
 import './element-ui'
 import './websocket'
 
-console.log('[DMX] 2018/10/20')
+console.log('[DMX] 2018/10/21')
 
 // 1) Init dm5 library
 // The dm5 library must be inited *before* the dm5-webclient component is created.
