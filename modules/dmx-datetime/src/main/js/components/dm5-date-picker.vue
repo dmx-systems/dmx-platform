0f65e4b4629ff3bcddd0fcd064293492e1ce66f0
Jörg Richter <jri@deepamehta.de>
2018-10-24 23:11:59 +0200

    Add field label to date picker (#54)

--- modules/dmx-datetime/src/main/js/components/dm5-date-picker.vue
+++ modules/dmx-datetime/src/main/js/components/dm5-date-picker.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="dm5-date-picker">
+    <div class="field-label">{{fieldLabel}}</div>
     <div v-if="infoMode">{{object.value}}</div>
     <el-date-picker v-else v-model="date"></el-date-picker>
   </div>
@@ -8,15 +9,35 @@
 <script>
 export default {
 
+  created () {
+    console.log('dm5-date-picker created', this.assocDef)
+  },
+
   mixins: [
     require('./mixins/object').default,       // object to render
     require('./mixins/info-mode').default,
+    require('./mixins/assoc-def').default,    // undefined for top-level object
     require('./mixins/context').default
   ],
 
   computed: {
 
+    type () {
+      return this.object.getType()
+    },
+
+    // TODO: copy in dm5-value-renderer.vue
+    fieldLabel () {
+      const customAssocType = this.assocDef && this.assocDef.getCustomAssocType()
+      return customAssocType && customAssocType.isSimple() ? customAssocType.value : this.type.value
+    },
+
+    mode () {
+      return this.context.mode
+    },
+
     date: {
+
       get () {
         console.log('date getter', this.object)
         const d = this.object.childs
@@ -26,6 +47,7 @@ export default {
           d['dmx.datetime.day'].value
         )
       },
+
       set (date) {
         console.log('date setter', date.getFullYear(), date.getMonth() + 1, date.getDate())
         const d = this.object.childs
@@ -33,10 +55,6 @@ export default {
         d['dmx.datetime.month'].value = date.getMonth() + 1
         d['dmx.datetime.day'].value   = date.getDate()
       }
-    },
-
-    mode () {
-      return this.context.mode
     }
   }
 }
---
+++ modules/dmx-datetime/src/main/js/components/mixins/assoc-def.js
@@ -0,0 +1,10 @@
+import dm5 from 'dm5'
+
+export default {
+  props: {
+    assocDef: {
+      type: dm5.AssocDef,
+      required: true
+    }
+  }
+}
