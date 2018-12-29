88ce46c2c1bb6f897974927910c9b9f91d7555aa
Jörg Richter <jri@deepamehta.de>
2018-12-29 14:59:34 +0100

    CI/CD: test auto-deploy

--- modules/dmx-webclient/src/main/js/main.js
+++ modules/dmx-webclient/src/main/js/main.js
@@ -9,7 +9,7 @@ import 'font-awesome/css/font-awesome.css'
 import './element-ui'
 import './websocket'
 
-console.log('[DMX] 2018/12/29')
+console.log('[DMX] 2018/12/29-2')
 
 // 1) Init dm5 library
 // The dm5 library must be inited *before* the dm5-webclient component is created.
