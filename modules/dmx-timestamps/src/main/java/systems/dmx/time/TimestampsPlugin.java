0c73cfafcf5f39185c0572fbf1b482fc6ead44d5
Jörg Richter <jri@deepamehta.de>
2019-03-09 01:04:13 +0100

    Consolidate Topicmaps data model, pt.1 (#161)
    Change the topicmap -> content association:
    
    As role types "dmx.core.parent" and "dmx.core.child" is used,
    the topicmap being the parent.
    
    Drop 1 role type:
    
    "Topicmap Content"
    dmx.topicmaps.topicmap_content

--- modules/dmx-timestamps/src/main/java/systems/dmx/time/TimestampsPlugin.java
+++ modules/dmx-timestamps/src/main/java/systems/dmx/time/TimestampsPlugin.java
@@ -28,10 +28,7 @@ import javax.ws.rs.core.MultivaluedMap;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Collection;
-import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Locale;
-import java.util.Set;
 import java.util.TimeZone;
 import java.util.logging.Logger;
 
--- modules/dmx-topicmaps/src/main/java/systems/dmx/topicmaps/TopicmapsConstants.java
+++ modules/dmx-topicmaps/src/main/java/systems/dmx/topicmaps/TopicmapsConstants.java
@@ -4,16 +4,19 @@ package systems.dmx.topicmaps;
 
 public interface TopicmapsConstants {
 
+    // content assoc
     static final String TOPICMAP_CONTEXT   = "dmx.topicmaps.topicmap_context";
-    static final String ROLE_TYPE_TOPICMAP = "dmx.core.default";
-    static final String ROLE_TYPE_CONTENT  = "dmx.topicmaps.topicmap_content";
+    static final String ROLE_TYPE_TOPICMAP = "dmx.core.parent";
+    static final String ROLE_TYPE_CONTENT  = "dmx.core.child";
 
-    static final String PROP_PAN_X      = "dmx.topicmaps.pan_x";
-    static final String PROP_PAN_Y      = "dmx.topicmaps.pan_y";
-    static final String PROP_ZOOM       = "dmx.topicmaps.zoom";
+    // topicmap props
+    static final String PROP_PAN_X         = "dmx.topicmaps.pan_x";
+    static final String PROP_PAN_Y         = "dmx.topicmaps.pan_y";
+    static final String PROP_ZOOM          = "dmx.topicmaps.zoom";
 
-    static final String PROP_X          = "dmx.topicmaps.x";
-    static final String PROP_Y          = "dmx.topicmaps.y";
-    static final String PROP_VISIBILITY = "dmx.topicmaps.visibility";
-    static final String PROP_PINNED     = "dmx.topicmaps.pinned";
+    // topic/assoc props
+    static final String PROP_X             = "dmx.topicmaps.x";    // topic only
+    static final String PROP_Y             = "dmx.topicmaps.y";    // topic only
+    static final String PROP_VISIBILITY    = "dmx.topicmaps.visibility";
+    static final String PROP_PINNED        = "dmx.topicmaps.pinned";
 }
