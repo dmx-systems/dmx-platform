bc75969bf19c5212402938923cc4959fa7d300ef
Jörg Richter <jri@deepamehta.de>
2019-07-14 17:12:12 +0200

    Add Help dialog content (#138)

--- modules/dmx-help-menu/src/main/js/components/dm5-help-dialog.vue
+++ modules/dmx-help-menu/src/main/js/components/dm5-help-dialog.vue
@@ -1,5 +1,36 @@
 <template>
-  <el-dialog custom-class="dm5-help-dialog" :visible="visible" title="Help" @close="close">
+  <el-dialog custom-class="dm5-help-dialog" :visible="visible" @close="close">
+    <div>
+      <img src="../dmx-logo.svg" width="85%">
+      <div style="display: flex">
+        <div>
+          <div class="field-label">Version</div>
+          5.0-beta-4-SNAPSHOT
+        </div>
+        <div style="margin-left: 3em">
+          <div class="field-label">Released</div>
+          July 14, 2019
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="field">
+        <div class="field-label">License</div>
+        GPL3 or later
+      </div>
+      <div class="field">
+        <div class="field-label">Copyright</div>
+        2019, <a href="https://dmx.systems">DMX Systems</a>
+      </div>
+      <div class="field">
+        <div class="field-label">Documentation</div>
+        <a href="https://docs.dmx.systems">docs.dmx.systems</a>
+      </div>
+      <div class="field">
+        <div class="field-label">Support</div>
+        <a href="https://forum.dmx.systems">forum.dmx.systems</a>
+      </div>
+    </div>
   </el-dialog>
 </template>
 
@@ -21,4 +52,21 @@ export default {
 </script>
 
 <style>
+.dm5-help-dialog .el-dialog__body {
+  display: flex;
+}
+
+.dm5-help-dialog .el-dialog__body > div:nth-child(1) {
+  flex-basis: 60%;
+}
+
+.dm5-help-dialog img {
+  position: relative;
+  top: -10px;
+  left: -15px;
+}
+
+.dm5-help-dialog .field + .field {
+  margin-top: var(--field-spacing);
+}
 </style>
