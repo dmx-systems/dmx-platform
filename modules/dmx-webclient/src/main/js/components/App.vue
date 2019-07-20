99c0be1994119c0923dc7505b989c42ada9798eb
Jörg Richter <jri@deepamehta.de>
2019-07-20 11:58:47 +0200

    Enable browser's own context menu again, 3rd try (#264)

--- modules/dmx-webclient/src/main/js/components/App.vue
+++ modules/dmx-webclient/src/main/js/components/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-view @contextmenu.native="contextmenu"></router-view>
+  <router-view @contextmenu.capture.native="contextmenu"></router-view>
   <!--
     Note: the Vue template compiler is not available as we use the Vue runtime.
     So we can't put <router-view> in index.html but must render the root component
@@ -11,6 +11,7 @@
 export default {
   methods: {
     contextmenu (e) {
+      console.log('contextmenu', e)
       if (e.target.tagName === 'CANVAS') {
         e.preventDefault()
       }
