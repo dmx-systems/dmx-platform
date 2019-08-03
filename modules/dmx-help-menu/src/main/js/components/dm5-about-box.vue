671f73e7af7497d8506415559c3f8a57f1890a04
Jörg Richter <jri@deepamehta.de>
2019-08-03 23:07:24 +0200

    Build system: update `exec-maven-plugin` to 1.6.0 (#275)

--- modules/dmx-help-menu/src/main/js/components/dm5-about-box.vue
+++ modules/dmx-help-menu/src/main/js/components/dm5-about-box.vue
@@ -8,7 +8,7 @@
       </div>
       <div class="field">
         <div class="field-label">Released</div>
-        Aug 1, 2019
+        Aug 3, 2019
       </div>
       <div class="field">
         <div class="field-label">License</div>
