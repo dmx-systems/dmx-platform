2eae99327549ce4ca08907773b5c8108a3ef63f1
Jörg Richter <jri@deepamehta.de>
2019-08-07 16:25:40 +0200

    Fix pom.xml: change license to AGPL (closes #262)

--- modules/dmx-webclient/src/main/js/main.js
+++ modules/dmx-webclient/src/main/js/main.js
@@ -9,7 +9,7 @@ import 'font-awesome/css/font-awesome.css'
 import './element-ui'
 import './websocket'
 
-console.log('[DMX] 2019/08/04')
+console.log('[DMX] 2019/08/07')
 
 // 1) Init dm5 library
 // The dm5 library must be inited *before* the dm5-webclient component is instantiated.
