e3249dbe9d839e21349ba4ab6f8e2a5d936857ac
Jörg Richter <jri@deepamehta.de>
2020-01-06 23:54:09 +0100

    Bump dependencies

--- modules/dmx-help-menu/src/main/js/components/dm5-about-box.vue
+++ modules/dmx-help-menu/src/main/js/components/dm5-about-box.vue
@@ -8,7 +8,7 @@
       </div>
       <div class="field">
         <div class="field-label">Released</div>
-        Dec 19, 2019
+        Jan 6, 2020
       </div>
       <div class="field">
         <div class="field-label">License</div>
