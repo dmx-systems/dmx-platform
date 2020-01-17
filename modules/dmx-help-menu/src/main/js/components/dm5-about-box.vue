1e7e4515e49e2a3f55a7d15aeec364a62d65691c
Jörg Richter <jri@deepamehta.de>
2020-01-17 22:07:30 +0100

    Downgrade to vue2-leaflet 2.2.1

--- modules/dmx-help-menu/src/main/js/components/dm5-about-box.vue
+++ modules/dmx-help-menu/src/main/js/components/dm5-about-box.vue
@@ -8,7 +8,7 @@
       </div>
       <div class="field">
         <div class="field-label">Released</div>
-        Jan 15, 2020
+        Jan 17, 2020
       </div>
       <div class="field">
         <div class="field-label">License</div>
