c0376e491cdee8befe6929c992b0650d4028df8b
Jörg Richter <jri@deepamehta.de>
2020-04-18 16:48:33 +0200

    CI/CD: activate platform tests (#249)

--- modules/dmx-help-menu/src/main/js/components/dm5-about-box.vue
+++ modules/dmx-help-menu/src/main/js/components/dm5-about-box.vue
@@ -8,7 +8,7 @@
       </div>
       <div class="field">
         <div class="field-label">Released</div>
-        Apr 17, 2020
+        Apr 18, 2020
       </div>
       <div class="field">
         <div class="field-label">License</div>
