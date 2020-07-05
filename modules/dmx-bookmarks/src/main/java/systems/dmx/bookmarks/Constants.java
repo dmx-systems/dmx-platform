94ff2ca754fdcc1e64ed478b49fc92e2c64ce781
Jörg Richter <jri@deepamehta.de>
2020-07-05 18:24:25 +0200

    Add "Bookmarks" URI constants (#379)

---
+++ modules/dmx-bookmarks/src/main/java/systems/dmx/bookmarks/Constants.java
@@ -0,0 +1,10 @@
+package systems.dmx.bookemarks;
+
+
+
+public class Constants {
+
+    // Topic Types
+    public static final String BOOKMARK             = "dmx.bookmarks.bookmark";
+    public static final String BOOKMARK_DESCRIPTION = "dmx.bookmarks.description";
+}
