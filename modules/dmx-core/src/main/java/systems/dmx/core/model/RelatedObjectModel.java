0cd35e10ca79041f198719b83c65dc8f5a46103d
Jörg Richter <jri@deepamehta.de>
2020-10-18 18:45:21 +0200

    Core API: add `getOtherDMXObject()` to `RelatedObjectModel` (#406)

--- modules/dmx-core/src/main/java/systems/dmx/core/model/RelatedObjectModel.java
+++ modules/dmx-core/src/main/java/systems/dmx/core/model/RelatedObjectModel.java
@@ -5,4 +5,6 @@ package systems.dmx.core.model;
 public interface RelatedObjectModel extends DMXObjectModel {
 
     AssocModel getRelatingAssoc();
+
+    <M extends DMXObjectModel> M getOtherDMXObject();
 }
