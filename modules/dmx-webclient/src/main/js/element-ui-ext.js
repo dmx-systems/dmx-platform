368a1b1189d31a2fe71399f62a2cf5734aef092a
Jörg Richter <jri@deepamehta.de>
2020-12-17 19:30:51 +0100

    Add `Drawer` and 4 more components to "UI Extensions" (#420)

--- modules/dmx-webclient/src/main/js/element-ui-ext.js
+++ modules/dmx-webclient/src/main/js/element-ui-ext.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import {
-  Table, TableColumn, Header, Container, Main, Upload
+  Table, TableColumn, Header, Container, Main, Upload,
+  Drawer, Timeline, TimelineItem, Badge, ButtonGroup
 } from 'element-ui'
 
 // register components
@@ -10,3 +11,8 @@ Vue.use(Header)
 Vue.use(Container)
 Vue.use(Main)
 Vue.use(Upload)
+Vue.use(Drawer)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
+Vue.use(Badge)
+Vue.use(ButtonGroup)
