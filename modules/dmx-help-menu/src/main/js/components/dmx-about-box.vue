1076f83cd79b03131932a6fe94ff7a80914d6329
Jörg Richter <jri@deepamehta.de>
2020-12-20 17:49:43 +0100

    Modify JS Standard Style linting rules (#422)

--- modules/dmx-help-menu/src/main/js/components/dmx-about-box.vue
+++ modules/dmx-help-menu/src/main/js/components/dmx-about-box.vue
@@ -9,7 +9,7 @@
       </div>
       <div class="field">
         <div class="field-label">Released</div>
-        Dec 17, 2020
+        Dec 20, 2020
       </div>
       <div class="field">
         <div class="field-label">License</div>
