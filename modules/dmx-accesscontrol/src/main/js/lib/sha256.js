cd1d203ad6162fa12633c145ed13911b2e8893c9
Jörg Richter <jri@deepamehta.de>
2020-12-22 14:46:42 +0100

    Adapt code to style guide, pt. 5 (#424)

--- modules/dmx-accesscontrol/src/main/js/lib/sha256.js
+++ modules/dmx-accesscontrol/src/main/js/lib/sha256.js
@@ -8,12 +8,12 @@
  */
 export default function SHA256 (s) {
 
-  var chrsz   = 8
-  var hexcase = 0
+  const chrsz   = 8
+  const hexcase = 0
 
   function safeAdd (x, y) {
-    var lsw = (x & 0xFFFF) + (y & 0xFFFF)
-    var msw = (x >> 16) + (y >> 16) + (lsw >> 16)
+    const lsw = (x & 0xFFFF) + (y & 0xFFFF)
+    const msw = (x >> 16) + (y >> 16) + (lsw >> 16)
     return (msw << 16) | (lsw & 0xFFFF)
   }
 
@@ -27,16 +27,24 @@ export default function SHA256 (s) {
   function Gamma1256 (x) { return (S(x, 17) ^ S(x, 19) ^ R(x, 10)) }
 
   function coreSHA256 (m, l) {
-    var K = [0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5, 0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5, 0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3, 0x72BE5D74, 0x80DEB1FE, 0x9BDC06A7, 0xC19BF174, 0xE49B69C1, 0xEFBE4786, 0xFC19DC6, 0x240CA1CC, 0x2DE92C6F, 0x4A7484AA, 0x5CB0A9DC, 0x76F988DA, 0x983E5152, 0xA831C66D, 0xB00327C8, 0xBF597FC7, 0xC6E00BF3, 0xD5A79147, 0x6CA6351, 0x14292967, 0x27B70A85, 0x2E1B2138, 0x4D2C6DFC, 0x53380D13, 0x650A7354, 0x766A0ABB, 0x81C2C92E, 0x92722C85, 0xA2BFE8A1, 0xA81A664B, 0xC24B8B70, 0xC76C51A3, 0xD192E819, 0xD6990624, 0xF40E3585, 0x106AA070, 0x19A4C116, 0x1E376C08, 0x2748774C, 0x34B0BCB5, 0x391C0CB3, 0x4ED8AA4A, 0x5B9CCA4F, 0x682E6FF3, 0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208, 0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2]
-    var HASH = [0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A, 0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19]
-    var W = new Array(64)
-    var a, b, c, d, e, f, g, h, i, j
-    var T1, T2
+    const K = [
+      0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5, 0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5,
+      0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3, 0x72BE5D74, 0x80DEB1FE, 0x9BDC06A7, 0xC19BF174,
+      0xE49B69C1, 0xEFBE4786, 0x0FC19DC6, 0x240CA1CC, 0x2DE92C6F, 0x4A7484AA, 0x5CB0A9DC, 0x76F988DA,
+      0x983E5152, 0xA831C66D, 0xB00327C8, 0xBF597FC7, 0xC6E00BF3, 0xD5A79147, 0x06CA6351, 0x14292967,
+      0x27B70A85, 0x2E1B2138, 0x4D2C6DFC, 0x53380D13, 0x650A7354, 0x766A0ABB, 0x81C2C92E, 0x92722C85,
+      0xA2BFE8A1, 0xA81A664B, 0xC24B8B70, 0xC76C51A3, 0xD192E819, 0xD6990624, 0xF40E3585, 0x106AA070,
+      0x19A4C116, 0x1E376C08, 0x2748774C, 0x34B0BCB5, 0x391C0CB3, 0x4ED8AA4A, 0x5B9CCA4F, 0x682E6FF3,
+      0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208, 0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2]
+    const HASH = [0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A, 0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19]
+    const W = new Array(64)
+    let a, b, c, d, e, f, g, h, i, j
+    let T1, T2
 
     m[l >> 5] |= 0x80 << (24 - l % 32)
     m[((l + 64 >> 9) << 4) + 15] = l
 
-    for (var i = 0; i < m.length; i += 16) {
+    for (i = 0; i < m.length; i += 16) {
       a = HASH[0]
       b = HASH[1]
       c = HASH[2]
@@ -46,7 +54,7 @@ export default function SHA256 (s) {
       g = HASH[6]
       h = HASH[7]
 
-      for (var j = 0; j < 64; j++) {
+      for (j = 0; j < 64; j++) {
         if (j < 16) W[j] = m[j + i]
         else W[j] = safeAdd(safeAdd(safeAdd(Gamma1256(W[j - 2]), W[j - 7]), Gamma0256(W[j - 15])), W[j - 16])
 
@@ -76,30 +84,28 @@ export default function SHA256 (s) {
   }
 
   function str2binb (str) {
-    var bin = Array()
-    var mask = (1 << chrsz) - 1
-    for(var i = 0; i < str.length * chrsz; i += chrsz) {
+    const bin = []
+    const mask = (1 << chrsz) - 1
+    for (let i = 0; i < str.length * chrsz; i += chrsz) {
       bin[i >> 5] |= (str.charCodeAt(i / chrsz) & mask) << (24 - i % 32)
     }
     return bin
   }
 
   function Utf8Encode (string) {
-    string = string.replace(/\r\n/g,'\n')
-    var utftext = ''
+    string = string.replace(/\r\n/g, '\n')
+    let utftext = ''
 
-    for (var n = 0; n < string.length; n++) {
+    for (let n = 0; n < string.length; n++) {
 
-      var c = string.charCodeAt(n)
+      const c = string.charCodeAt(n)
 
       if (c < 128) {
         utftext += String.fromCharCode(c)
-      }
-      else if((c > 127) && (c < 2048)) {
+      } else if ((c > 127) && (c < 2048)) {
         utftext += String.fromCharCode((c >> 6) | 192)
         utftext += String.fromCharCode((c & 63) | 128)
-      }
-      else {
+      } else {
         utftext += String.fromCharCode((c >> 12) | 224)
         utftext += String.fromCharCode(((c >> 6) & 63) | 128)
         utftext += String.fromCharCode((c & 63) | 128)
@@ -111,9 +117,9 @@ export default function SHA256 (s) {
   }
 
   function binb2hex (binarray) {
-    var hexTab = hexcase ? '0123456789ABCDEF' : '0123456789abcdef'
-    var str = ''
-    for(var i = 0; i < binarray.length * 4; i++) {
+    const hexTab = hexcase ? '0123456789ABCDEF' : '0123456789abcdef'
+    let str = ''
+    for (let i = 0; i < binarray.length * 4; i++) {
       str += hexTab.charAt((binarray[i >> 2] >> ((3 - i % 4) * 8 + 4)) & 0xF) +
       hexTab.charAt((binarray[i >> 2] >> ((3 - i % 4) * 8)) & 0xF)
     }
