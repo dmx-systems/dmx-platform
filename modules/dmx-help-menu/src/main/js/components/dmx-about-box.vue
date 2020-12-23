6bc8943c2b57ea2ffede32832c4d5fe35afe85f7
Jörg Richter <jri@deepamehta.de>
2020-12-23 14:26:02 +0100

    Modify linting config, pt. 6 (even more `ignorePatterns`) (#422)

--- modules/dmx-help-menu/src/main/js/components/dmx-about-box.vue
+++ modules/dmx-help-menu/src/main/js/components/dmx-about-box.vue
@@ -9,7 +9,7 @@
       </div>
       <div class="field">
         <div class="field-label">Released</div>
-        Dec 22, 2020
+        Dec 23, 2020
       </div>
       <div class="field">
         <div class="field-label">License</div>
