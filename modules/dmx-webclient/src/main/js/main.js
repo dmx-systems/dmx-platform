28e60750dcc138b03a0458b3ef3446a56e99f43b
Jörg Richter <jri@deepamehta.de>
2020-12-24 13:22:00 +0100

    Modify linting config, pt. 7 (universal `ignorePatterns`) (#422)

--- modules/dmx-webclient/src/main/js/main.js
+++ modules/dmx-webclient/src/main/js/main.js
@@ -8,7 +8,7 @@ import onHttpError from './error-handler'
 import extraElementUI from './element-ui'
 import './websocket'
 
-console.log('[DMX] 2020/12/23')
+console.log('[DMX] 2020/12/24')
 
 // 1) Init dmx library
 // The dmx library must be inited *before* the dmx-webclient component is instantiated.
