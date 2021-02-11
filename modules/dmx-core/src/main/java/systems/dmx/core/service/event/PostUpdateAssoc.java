a196d1e009d19e70ced21d46c765208ef627df32
Jörg Richter <jri@deepamehta.de>
2021-02-11 14:43:09 +0100

    Add `ChangeReport` param to postUpdate handlers (#60)

--- modules/dmx-core/src/main/java/systems/dmx/core/service/event/PostUpdateAssoc.java
+++ modules/dmx-core/src/main/java/systems/dmx/core/service/event/PostUpdateAssoc.java
@@ -2,11 +2,12 @@ package systems.dmx.core.service.event;
 
 import systems.dmx.core.Assoc;
 import systems.dmx.core.model.AssocModel;
+import systems.dmx.core.service.ChangeReport;
 import systems.dmx.core.service.EventListener;
 
 
 
 public interface PostUpdateAssoc extends EventListener {
 
-    void postUpdateAssoc(Assoc assoc, AssocModel updateModel, AssocModel oldAssoc);
+    void postUpdateAssoc(Assoc assoc, AssocModel updateModel, AssocModel oldAssoc, ChangeReport report);
 }
