73fba9e98c365ac783d189a023ce34f2de7085b4
Jörg Richter <jri@deepamehta.de>
2021-03-02 12:08:35 +0100

    Bump `dmx-api` dependency to SNAPSHOT (#438)

--- modules/dmx-help-menu/src/main/js/components/dmx-about-box.vue
+++ modules/dmx-help-menu/src/main/js/components/dmx-about-box.vue
@@ -9,7 +9,7 @@
       </div>
       <div class="field">
         <div class="field-label">Released</div>
-        Feb 28, 2021
+        Mar 2, 2021
       </div>
       <div class="field">
         <div class="field-label">License</div>
