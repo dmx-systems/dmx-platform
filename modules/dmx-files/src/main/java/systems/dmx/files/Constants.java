a8507e6cc0cbd714e5c53c14ecc2ab1b2dc1c94c
Jörg Richter <jri@deepamehta.de>
2021-03-14 14:57:16 +0100

    Add Files API constants (#438, #360)

---
+++ modules/dmx-files/src/main/java/systems/dmx/files/Constants.java
@@ -0,0 +1,16 @@
+package systems.dmx.files;
+
+
+
+public class Constants {
+
+    // Topic Types
+    public static final String FILE        = "dmx.files.file";
+    public static final String FILE_NAME   = "dmx.files.file_name";
+    public static final String FOLDER      = "dmx.files.folder";
+    public static final String FOLDER_NAME = "dmx.files.folder_name";
+    public static final String PATH        = "dmx.files.path";
+    public static final String MEDIA_TYPE  = "dmx.files.media_type";
+    public static final String SIZE        = "dmx.files.size";
+    public static final String DISK_QUOTA  = "dmx.files.disk_quota";
+}
