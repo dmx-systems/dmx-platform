66a1d1330ecd14bd8f2e4ceff49c146b562c99e0
Jörg Richter <jri@deepamehta.de>
2021-04-01 16:58:47 +0200

    Workspaces module: add missing URI constants (#444, #360)

--- modules/dmx-workspaces/src/main/java/systems/dmx/workspaces/Constants.java
+++ modules/dmx-workspaces/src/main/java/systems/dmx/workspaces/Constants.java
@@ -11,6 +11,13 @@ public class Constants {
     public static final String WORKSPACE_FACET       = "dmx.workspaces.workspace_facet";
     public static final String SHARING_MODE          = "dmx.workspaces.sharing_mode";
 
+    public static final String ENABLED_SHARING_MODES = "dmx.workspaces.enabled_sharing_modes";
+    public static final String PRIVATE_ENABLED       = "dmx.workspaces.private.enabled";
+    public static final String CONFIDENTIAL_ENABLED  = "dmx.workspaces.confidential.enabled";
+    public static final String COLLABORATIVE_ENABLED = "dmx.workspaces.collaborative.enabled";
+    public static final String PUBLIC_ENABLED        = "dmx.workspaces.public.enabled";
+    public static final String COMMON_ENABLED        = "dmx.workspaces.common.enabled";
+
     // Assoc Types
     public static final String WORKSPACE_ASSIGNMENT  = "dmx.workspaces.workspace_assignment";
 }
