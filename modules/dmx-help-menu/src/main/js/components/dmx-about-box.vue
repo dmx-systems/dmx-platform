3c9eb1c9533d9de330af5c147f612387228e8e40
Jörg Richter <jri@deepamehta.de>
2021-06-17 15:58:32 +0200

    Add security hints to config file (#454)

--- modules/dmx-help-menu/src/main/js/components/dmx-about-box.vue
+++ modules/dmx-help-menu/src/main/js/components/dmx-about-box.vue
@@ -9,7 +9,7 @@
       </div>
       <div class="field">
         <div class="field-label">Released</div>
-        Jun 13, 2021
+        Jun 17, 2021
       </div>
       <div class="field">
         <div class="field-label">License</div>
