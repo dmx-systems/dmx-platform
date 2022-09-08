5db0665e0bdd09ef9927b1a2d67cde40e4f3dba6
Jörg Richter <jri@deepamehta.de>
2022-09-08 21:08:08 +0200

    Adapt to changed `dmx-api` module (auto-websocket)
    See dmx-projects/zukunftswerk#155

--- modules/dmx-webclient/src/main/js/main.js
+++ modules/dmx-webclient/src/main/js/main.js
@@ -6,9 +6,12 @@ import router from './router'
 import loadPlugins from './plugin-manager'
 import onHttpError from './error-handler'
 import extraElementUI from './element-ui'
-import './websocket'
 
-console.log('[DMX] 2022/09/08')
+console.log('[DMX] 2022/09/08-2')
+
+const messageHandler = message => {
+  store.dispatch('_' + message.type, message.args)    // FIXME: use message bus instead of actions
+}
 
 // 1) Init dmx library
 // The dmx library must be inited *before* the dmx-webclient component is instantiated.
@@ -16,6 +19,7 @@ console.log('[DMX] 2022/09/08')
 const typeCacheReady = dmx.init({
   topicTypes: 'all',
   store,
+  messageHandler,
   onHttpError,
   iconRenderers: store.state.iconRenderers
 })
--- modules/dmx-webclient/src/main/js/websocket.js
+++
@@ -1,8 +0,0 @@
-import DMXWebSocket from 'dmx-websocket'
-import store from './store/webclient'
-
-/* eslint no-new: 0 */
-
-new DMXWebSocket(message => {
-  store.dispatch('_' + message.type, message.args)
-})
