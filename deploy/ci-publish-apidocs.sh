be849c091e4e41fe2fde44b0a4bed124e18c91c4
Juergen Neumann <juergen.neumann@dmx.berlin>
2024-03-08 11:49:54 +0100

    CI: wip - improve apidocs deployment

--- deploy/ci-publish-apidocs.sh
+++ deploy/ci-publish-apidocs.sh
@@ -7,7 +7,7 @@
 
 
 ## get job id of build job (required to access artifacts)
-RESULT="$( curl -sS --header "JOB_TOKEN: ${CI_JOB_TOKEN}" "https://git.dmx.systems/api/v4/projects/${CI_PROJECT_ID}/pipelines/${CI_PIPELINE_ID}/jobs?scope[]=running" )"
+RESULT="$( curl -sS --header "JOB_TOKEN: ${CI_JOB_TOKEN}" "https://git.dmx.systems/api/v4/projects/${CI_PROJECT_ID}/pipelines/${CI_PIPELINE_ID}/jobs?scope[]=success" )"
 MAVEN_BUILD_JOB_ID="$( echo "${RESULT}" | jq -c '.[] | select(.name | contains("javadoc")).id' )"
 if [ -z "${MAVEN_BUILD_JOB_ID##*[!0-9]*}" ]; then
     echo "ERROR! Could not get job id from 'javadoc' job. (MAVEN_BUILD_JOB_ID=${MAVEN_BUILD_JOB_ID})"
