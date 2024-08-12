126a8a10d939ae3a1e4a48f1cfdabcdbcece7c88
Jörg Richter <jri@deepamehta.de>
2024-08-11 18:23:15 -0600

    Fix: skip `admin` account creation on platform update (#544)

--- modules/dmx-account-management/src/main/java/systems/dmx/accountmanagement/migrations/Migration1.java
+++ modules/dmx-account-management/src/main/java/systems/dmx/accountmanagement/migrations/Migration1.java
@@ -6,6 +6,9 @@ import systems.dmx.core.service.Inject;
 import systems.dmx.core.service.Migration;
 import systems.dmx.core.service.accesscontrol.Credentials;
 
+import java.util.logging.Logger;
+
+
 
 /**
  * Creates "admin" user account.
@@ -22,13 +25,23 @@ public class Migration1 extends Migration {
     @Inject
     private AccountManagementService accountManagementService;
 
+    @Inject
+    private AccessControlService accessControlService;
+
+    private Logger logger = Logger.getLogger(getClass().getName());
+
     // -------------------------------------------------------------------------------------------------- Public Methods
 
     @Override
     public void run() {
-        accountManagementService.createUserAccount(new Credentials(
-            AccessControlService.ADMIN_USERNAME,
-            AccessControlService.ADMIN_INITIAL_PASSWORD
-        ));
+        if (accessControlService.getUsernameTopic(AccessControlService.ADMIN_USERNAME) == null) {
+            accountManagementService.createUserAccount(new Credentials(
+                AccessControlService.ADMIN_USERNAME,
+                AccessControlService.ADMIN_INITIAL_PASSWORD
+            ));
+        } else {
+            // if platform is *updated* to 5.3.5 or later the admin account was created by dmx-accesscontrol already
+            logger.info("### Creating admin account SKIPPED -- exists already");
+        }
     }
 }
